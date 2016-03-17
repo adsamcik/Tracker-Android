18180d39ca5af2564c7272298980d7f7b8f2837f
adsamcik <adsamcik@gmail.com>
2016-03-17 19:13:19 +0100

    Crash fix

--- app/src/main/java/com/adsamcik/signalcollector/Receivers/ConnectivityChangeReceiver.java
+++ app/src/main/java/com/adsamcik/signalcollector/Receivers/ConnectivityChangeReceiver.java
@@ -3,15 +3,17 @@ package com.adsamcik.signalcollector.Receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import com.adsamcik.signalcollector.DataStore;
+import com.adsamcik.signalcollector.Extensions;
 
 public class ConnectivityChangeReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		Log.d("tag", intent.getAction());
+		Extensions.Initialize((TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE));
 		DataStore.updateAutoUploadState(context);
 	}
 }
