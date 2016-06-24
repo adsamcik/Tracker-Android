a6f568f1b47971662c4bfae811db8c771fdaaa03
adsamcik <adsamcik@users.noreply.github.com>
2016-06-24 18:21:08 +0200

    Replaced acra with firebase crash reporting

--- app/src/main/java/com/adsamcik/signalcollector/App.java
+++ app/src/main/java/com/adsamcik/signalcollector/App.java
@@ -2,17 +2,10 @@ package com.adsamcik.signalcollector;
 
 import android.app.Application;
 
-import org.acra.ACRA;
-import org.acra.annotation.ReportsCrashes;
-
-@ReportsCrashes(
-		formUri = "http://collector.adsamcik.xyz/report.php"
-)
 public class App extends Application {
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		ACRA.init(this);
 	}
 }
