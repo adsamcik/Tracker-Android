87dd7f7a8695526075a85e87754a80b56f0d21e9
adsamcik <adsamcik@users.noreply.github.com>
2016-07-23 16:17:02 +0200

    Refactoring

--- app/src/main/java/com/adsamcik/signalcollector/NoiseTracking.java
+++ app/src/main/java/com/adsamcik/signalcollector/NoiseTracking.java
@@ -6,17 +6,13 @@ import android.media.MediaRecorder;
 import android.util.Log;
 
 public class NoiseTracking {
-	public static double REFERENCE = 0.00002;
-	public static final int SAMPLING = 44100;
-	static final int bufferSize = AudioRecord.getMinBufferSize(SAMPLING, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
-	static final AudioRecord audioRecorder = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLING, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, 2 * bufferSize);
+	private static final String TAG = "SignalsNoise";
+	private static double REFERENCE = 0.00002;
+	private static final int SAMPLING = 44100;
+	private static final int bufferSize = AudioRecord.getMinBufferSize(SAMPLING, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+	private static final AudioRecord audioRecorder = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLING, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, 2 * bufferSize);
 
-	public static void Record() {
-		audioRecorder.startRecording();
-	}
-
-
-	public static double getAmplitude() {
+	private static double getAmplitude() {
 		short[] buffer = new short[bufferSize];
 		audioRecorder.read(buffer, 0, bufferSize);
 		int max = 0;
@@ -25,12 +21,16 @@ public class NoiseTracking {
 				max = Math.abs(s);
 			}
 		}
-		Log.d("silence", "max " + max);
+		Log.d(TAG, "max " + max);
 		return max;
 	}
 
 	public static double GetNoiseLevel() {
 		return 20 * Math.log10(Math.abs(getAmplitude()));
+	}
+
+	public static void Record() {
+		audioRecorder.startRecording();
 	}
 
 	public static void Stop() {
