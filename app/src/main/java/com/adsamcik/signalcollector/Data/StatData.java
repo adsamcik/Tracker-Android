44f197fc8458c2f92b362d64b97c6ede9edf5a13
adsamcik <adsamcik@users.noreply.github.com>
2016-07-25 15:33:30 +0200

    Updated access modifier

--- app/src/main/java/com/adsamcik/signalcollector/Data/StatData.java
+++ app/src/main/java/com/adsamcik/signalcollector/Data/StatData.java
@@ -4,7 +4,7 @@ public class StatData {
 	public final String id;
 	public final String value;
 
-	public StatData(String id, String value) {
+	StatData(String id, String value) {
 		this.id = id;
 		this.value = value;
 	}
