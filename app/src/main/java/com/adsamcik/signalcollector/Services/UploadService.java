cb2d874efce38c382cc2b0ed548c99c0ac1fd459
adsamcik <adsamcik@users.noreply.github.com>
2016-08-05 13:48:24 +0200

    Converted callbacks to lambda

--- app/src/main/java/com/adsamcik/signalcollector/Services/UploadService.java
+++ app/src/main/java/com/adsamcik/signalcollector/Services/UploadService.java
@@ -70,29 +70,23 @@ public class UploadService extends JobService {
 				"\"data\":" + data + "}";
 
 		StringRequest postRequest = new StringRequest(Request.Method.POST, Network.URL_DATA_UPLOAD,
-				new Response.Listener<String>() {
-					@Override
-					public void onResponse(String response) {
-						/*try {
-							JSONObject jsonResponse = new JSONObject(response).getJSONObject("form");
-							String site = jsonResponse.getString("site"),
-									network = jsonResponse.getString("network");
-							System.out.println("Site: "+site+"\nNetwork: "+network);
-						} catch (JSONException e) {
-							e.printStackTrace();
-						}*/
-						//deleteFile(name);
-						TrackerService.approxSize -= size;
-						DataStore.onUpload();
-						checkQueue();
-					}
+				response -> {
+					/*try {
+						JSONObject jsonResponse = new JSONObject(response).getJSONObject("form");
+						String site = jsonResponse.getString("site"),
+								network = jsonResponse.getString("network");
+						System.out.println("Site: "+site+"\nNetwork: "+network);
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}*/
+					//deleteFile(name);
+					TrackerService.approxSize -= size;
+					DataStore.onUpload();
+					checkQueue();
 				},
-				new Response.ErrorListener() {
-					@Override
-					public void onErrorResponse(VolleyError error) {
-						DataStore.requestUpload(context, true);
-						checkQueue();
-					}
+				error -> {
+					DataStore.requestUpload(context, true);
+					checkQueue();
 				}
 		) {
 			@Override
