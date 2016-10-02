1747b867eab72be3c60d8968f3418ab7e47daf38
adsamcik <adsamcik@users.noreply.github.com>
2016-10-02 16:01:21 +0200

    Update variable names in UploadStats

--- app/src/main/java/com/adsamcik/signalcollector/classes/UploadStats.java
+++ app/src/main/java/com/adsamcik/signalcollector/classes/UploadStats.java
@@ -1,14 +1,17 @@
 package com.adsamcik.signalcollector.classes;
 
 public class UploadStats {
-	public final int newWifi, newCell, cell, wifi, locations, noiseCollections;
+	public final int newWifi, newCell, cell, wifi, collections, noiseCollections, newLocations;
+	public final long uploadSize;
 
-	public UploadStats(int newWifi, int newCell, int cell, int wifi, int locations, int noiseCollections) {
+	public UploadStats(int wifi, int newWifi, int cell, int newCell, int collections, int newLocations, int noiseCollections, long uploadSize) {
 		this.newCell = newCell;
 		this.newWifi = newWifi;
 		this.cell = cell;
 		this.wifi = wifi;
-		this.locations = locations;
+		this.collections = collections;
 		this.noiseCollections = noiseCollections;
+		this.uploadSize = uploadSize;
+		this.newLocations = newLocations;
 	}
 }
