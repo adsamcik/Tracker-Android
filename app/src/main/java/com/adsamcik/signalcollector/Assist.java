39829b56f3a7d1870cab4c6dd3b3bf22bb821442
adsamcik <adsamcik@users.noreply.github.com>
2016-10-13 22:55:48 +0200

    Add check for emulators

--- app/src/main/java/com/adsamcik/signalcollector/Assist.java
+++ app/src/main/java/com/adsamcik/signalcollector/Assist.java
@@ -387,4 +387,15 @@ public class Assist {
 	public static int getAgeInDays(long time) {
 		return (int) ((System.currentTimeMillis() - time) / DAY_IN_MILLISECONDS);
 	}
+
+	public static boolean isEmulator() {
+		return Build.FINGERPRINT.startsWith("generic")
+				|| Build.FINGERPRINT.startsWith("unknown")
+				|| Build.MODEL.contains("google_sdk")
+				|| Build.MODEL.contains("Emulator")
+				|| Build.MODEL.contains("Android SDK built for x86")
+				|| Build.MANUFACTURER.contains("Genymotion")
+				|| (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+				|| "google_sdk".equals(Build.PRODUCT);
+	}
 }
