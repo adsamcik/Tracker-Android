c572f56595cb0214e12d957484297b7b074cdee9
adsamcik <adsamcik@users.noreply.github.com>
2016-11-20 11:26:06 +0100

    Refactoring

--- app/src/main/java/com/adsamcik/signalcollector/utility/Shortcuts.java
+++ app/src/main/java/com/adsamcik/signalcollector/utility/Shortcuts.java
@@ -3,22 +3,15 @@ package com.adsamcik.signalcollector.utility;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.ShortcutInfo;
 import android.content.pm.ShortcutManager;
-import android.graphics.Color;
 import android.graphics.drawable.Icon;
-import android.net.Uri;
-import android.os.Build;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.adsamcik.signalcollector.R;
-import com.adsamcik.signalcollector.activities.IntroActivity;
-import com.adsamcik.signalcollector.activities.MainActivity;
 import com.adsamcik.signalcollector.activities.ShortcutActivity;
-import com.adsamcik.signalcollector.receivers.ShortcutReceiver;
 import com.adsamcik.signalcollector.services.TrackerService;
 
 import java.util.ArrayList;
@@ -28,7 +21,7 @@ import java.util.List;
 public class Shortcuts {
 	public static final String TRACKING_ID = "Tracking";
 	public static final String ACTION = "com.adsamcik.signalcollector.SHORTCUT";
-	private static ShortcutReceiver shortcutReceiver = null;
+	public static final String ACTION_STRING = "ShortcutAction";
 
 	/**
 	 * Initializes shortcuts
@@ -56,7 +49,7 @@ public class Shortcuts {
 		ShortcutInfo.Builder shortcutBuilder = new ShortcutInfo.Builder(context, id)
 				.setShortLabel(shortLabel)
 				.setIcon(Icon.createWithResource(context, iconResource))
-				.setIntent(new Intent(context, ShortcutActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP).setAction(ACTION).putExtra(ShortcutReceiver.ACTION_STRING, action.ordinal()));
+				.setIntent(new Intent(context, ShortcutActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP).setAction(ACTION).putExtra(ACTION_STRING, action.ordinal()));
 		if (longLabel != null)
 			shortcutBuilder.setLongLabel(longLabel);
 		return shortcutBuilder.build();
