d5e6537605623c72973352cd2d50d6ba4f176e26
adsamcik <adsamcik@users.noreply.github.com>
2016-11-20 13:05:06 +0100

    Add exceptions to shortcut activity

--- app/src/main/java/com/adsamcik/signalcollector/activities/ShortcutActivity.java
+++ app/src/main/java/com/adsamcik/signalcollector/activities/ShortcutActivity.java
@@ -2,11 +2,14 @@ package com.adsamcik.signalcollector.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import com.adsamcik.signalcollector.services.TrackerService;
+import com.adsamcik.signalcollector.utility.NotYetImplementedException;
 import com.adsamcik.signalcollector.utility.Shortcuts;
 import com.adsamcik.signalcollector.utility.Shortcuts.ShortcutType;
+import com.google.firebase.crash.FirebaseCrash;
 
 public class ShortcutActivity extends Activity {
 
@@ -29,7 +32,11 @@ public class ShortcutActivity extends Activity {
 						if (TrackerService.isRunning())
 							stopService(serviceIntent);
 						break;
+					default:
+						throw new NotYetImplementedException(type.name() + " is not implemented.");
 				}
+			} else {
+				FirebaseCrash.report(new Throwable("Invalid value " + value));
 			}
 		}
 		finishAffinity();
