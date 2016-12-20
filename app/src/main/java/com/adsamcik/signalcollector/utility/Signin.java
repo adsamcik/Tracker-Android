b5d1cd38faf89f5f632c869f5f505fed22b423b3
adsamcik <adsamcik@gmail.com>
2016-12-20 17:18:29 +0100

    Improvements to Signin

--- app/src/main/java/com/adsamcik/signalcollector/utility/Signin.java
+++ app/src/main/java/com/adsamcik/signalcollector/utility/Signin.java
@@ -3,12 +3,14 @@ package com.adsamcik.signalcollector.utility;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.adsamcik.signalcollector.R;
+import com.google.android.gms.auth.GoogleAuthUtil;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.ConnectionResult;
@@ -19,7 +21,7 @@ import java.lang.ref.WeakReference;
 
 
 public class Signin implements GoogleApiClient.OnConnectionFailedListener {
-	private static final String TOKEN = "userTOKEN";
+	private static final String TOKEN = "kNeoeSe";
 
 	public static final int RC_SIGN_IN = 4654;
 	private final GoogleApiClient client;
@@ -27,23 +29,26 @@ public class Signin implements GoogleApiClient.OnConnectionFailedListener {
 	private WeakReference<Button> signOutButton;
 	private final WeakReference<FragmentActivity> activityWeakReference;
 
-	private static Signin instance;
+	private static Signin instance = null;
 
 	private Activity getActivity() {
 		return activityWeakReference != null ? activityWeakReference.get() : null;
 	}
 
-	public static Signin getInstance(FragmentActivity fragmentActivity) {
-		if (instance == null)
+	public static Signin getInstance(@Nullable FragmentActivity fragmentActivity) {
+		if (instance == null && fragmentActivity != null)
 			instance = new Signin(fragmentActivity);
 		return instance;
+	}
+
+	public static String getToken() {
+		return Preferences.get().getString(TOKEN, null);
 	}
 
 	private Signin(@NonNull FragmentActivity activity) {
 		activityWeakReference = new WeakReference<>(activity);
 		GoogleSignInOptions gso = new GoogleSignInOptions.Builder()
 				.requestIdToken(activity.getResources().getString(R.string.server_client_id))
-				.requestId()
 				.build();
 		client = new GoogleApiClient.Builder(activity)
 				.enableAutoManage(activity, this)
@@ -88,9 +93,10 @@ public class Signin implements GoogleApiClient.OnConnectionFailedListener {
 		}
 	}
 
-	public void onSignedIn() {
+	public void onSignedIn(@NonNull String token) {
 		updateButtons(true);
 		showSnackbar(R.string.signed_in_message);
+		Preferences.get().edit().putString(TOKEN, token).apply();
 	}
 
 	private void onSignedOut() {
@@ -106,6 +112,7 @@ public class Signin implements GoogleApiClient.OnConnectionFailedListener {
 
 	private void revokeAccess() {
 		Auth.GoogleSignInApi.revokeAccess(client).setResultCallback(status -> onSignedOut());
+		Preferences.get().edit().remove(TOKEN).apply();
 	}
 
 	@Override
@@ -115,9 +122,5 @@ public class Signin implements GoogleApiClient.OnConnectionFailedListener {
 			Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(client);
 			activityWeakReference.get().startActivityForResult(signInIntent, RC_SIGN_IN);
 		}
-	}
-
-	public String getToken() {
-		return Preferences.get().getString(TOKEN, null);
 	}
 }
