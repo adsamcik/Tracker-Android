791926a2cb5bd439bd5e896f57e961437e9b656c
adsamcik <adsamcik@gmail.com>
2017-04-05 18:05:45 +0200

    Initial draft of noise testing activity

---
+++ app/src/main/java/com/adsamcik/signalcollector/activities/NoiseTestingActivity.java
@@ -0,0 +1,15 @@
+package com.adsamcik.signalcollector.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.adsamcik.signalcollector.R;
+
+public class NoiseTestingActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_noise_testing);
+	}
+}
