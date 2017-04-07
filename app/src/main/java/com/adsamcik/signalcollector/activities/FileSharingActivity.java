f7f2eb17e0cfe6ef11a6a7bb6bc83472a73b043f
adsamcik <adsamcik@gmail.com>
2017-04-07 21:39:19 +0200

    Fix #42

--- app/src/main/java/com/adsamcik/signalcollector/activities/FileSharingActivity.java
+++ app/src/main/java/com/adsamcik/signalcollector/activities/FileSharingActivity.java
@@ -3,15 +3,27 @@ package com.adsamcik.signalcollector.activities;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.FileProvider;
+import android.util.Log;
 import android.util.SparseBooleanArray;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.adsamcik.signalcollector.R;
+import com.adsamcik.signalcollector.utility.Assist;
+import com.adsamcik.signalcollector.utility.BottomSheetMenu;
 import com.adsamcik.signalcollector.utility.Compress;
 import com.adsamcik.signalcollector.utility.DataStore;
 import com.adsamcik.signalcollector.utility.SnackMaker;
@@ -30,52 +42,73 @@ public class FileSharingActivity extends DetailActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		files = getFilesDir().listFiles((dir1, name) -> name.startsWith(DataStore.DATA_FILE));
-		fileNames = new String[files.length];
-		for (int i = 0; i < fileNames.length; i++)
-			fileNames[i] = files[i].getName();
+		if (files.length == 0) {
+			TextView tv = new TextView(this);
+			tv.setText(R.string.share_nothing_to_share);
+			tv.setGravity(Gravity.CENTER_HORIZONTAL);
+			createContentParent(true).addView(tv);
+		} else {
+			fileNames = new String[20];
+			for (int i = 0; i < fileNames.length; i++)
+				fileNames[i] = files[0].getName();
 
-		LinearLayout parent = createContentParent(false);
-		LinearLayout ll = (LinearLayout) getLayoutInflater().inflate(R.layout.layout_file_share, parent);
-		ListView listView = (ListView) ll.findViewById(R.id.share_list_view);
-		ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, fileNames);
-		listView.setAdapter(adapter);
-		listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+			LinearLayout parent = createContentParent(false);
+			CoordinatorLayout layout = (CoordinatorLayout) ((ViewGroup) getLayoutInflater().inflate(R.layout.layout_file_share, parent)).getChildAt(parent.getChildCount() - 1);
+			ListView listView = (ListView) layout.findViewById(R.id.share_list_view);
+			ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, fileNames);
+			listView.setAdapter(adapter);
+			listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
 
-		Button shareButton = (Button) ll.findViewById(R.id.share_confirm_button);
-		shareButton.setText(getString(R.string.export_share_button).toUpperCase());
-		shareButton.setOnClickListener(v -> {
-			SparseBooleanArray sba = listView.getCheckedItemPositions();
-			ArrayList<String> temp = new ArrayList<>();
-			int len = listView.getCount();
-			for (int i = 0; i < len; i++)
-				if (sba.get(i))
-					temp.add(fileNames[i]);
+			View.OnClickListener shareOnClickListener = v -> {
+				SparseBooleanArray sba = listView.getCheckedItemPositions();
+				ArrayList<String> temp = new ArrayList<>();
+				for (int i = 1; i < fileNames.length; i++)
+					if (sba.get(i))
+						temp.add(fileNames[i]);
 
-			String[] arr = new String[temp.size()];
-			temp.toArray(arr);
-			File c = Compress.zip(files[0].getParent(), arr, "export_" + System.currentTimeMillis());
-			File target = new File(c.getParent() + File.separatorChar + SHAREABLE_DIR_NAME + File.separatorChar + c.getName() + ".zip");
-			shareableDir = new File(c.getParent() + File.separatorChar + SHAREABLE_DIR_NAME);
-			if (shareableDir.exists() || shareableDir.mkdir()) {
-				if (c.renameTo(target)) {
-					Uri fileUri = FileProvider.getUriForFile(
-							FileSharingActivity.this,
-							"com.asdamcik.signalcollector.fileprovider",
-							target);
-					Intent shareIntent = new Intent();
-					shareIntent.setAction(Intent.ACTION_SEND);
-					shareIntent.putExtra(Intent.EXTRA_STREAM, fileUri);
-					shareIntent.setType("application/zip");
-					startActivityForResult(Intent.createChooser(shareIntent, getResources().getText(R.string.export_share_button)), SHARE_RESULT);
+				if (temp.size() == 0)
+					new SnackMaker(layout).showSnackbar(R.string.share_nothing_to_share, Snackbar.LENGTH_SHORT);
+				else {
+					String[] arr = new String[temp.size()];
+					temp.toArray(arr);
+					File c = Compress.zip(files[0].getParent(), arr, "export_" + System.currentTimeMillis());
+					File target = new File(c.getParent() + File.separatorChar + SHAREABLE_DIR_NAME + File.separatorChar + c.getName() + ".zip");
+					shareableDir = new File(c.getParent() + File.separatorChar + SHAREABLE_DIR_NAME);
+					if (shareableDir.exists() || shareableDir.mkdir()) {
+						if (c.renameTo(target)) {
+							Uri fileUri = FileProvider.getUriForFile(
+									FileSharingActivity.this,
+									"com.asdamcik.signalcollector.fileprovider",
+									target);
+							Intent shareIntent = new Intent();
+							shareIntent.setAction(Intent.ACTION_SEND);
+							shareIntent.putExtra(Intent.EXTRA_STREAM, fileUri);
+							shareIntent.setType("application/zip");
+							startActivityForResult(Intent.createChooser(shareIntent, getResources().getText(R.string.export_share_button)), SHARE_RESULT);
 
-				} else
-					new SnackMaker(parent).showSnackbar("Failed to rename to " + target.getPath().substring(30));
-			} else
-				new SnackMaker(parent).showSnackbar("Failed to create dirs");
+						} else
+							new SnackMaker(parent).showSnackbar("Failed to rename to " + target.getPath().substring(30));
+					} else
+						new SnackMaker(parent).showSnackbar("Failed to create dirs");
 
-			target.deleteOnExit();
-			shareableDir.deleteOnExit();
-		});
+					target.deleteOnExit();
+					shareableDir.deleteOnExit();
+				}
+			};
+
+			BottomSheetMenu bottomSheetMenu = new BottomSheetMenu(layout);
+			bottomSheetMenu.addItem(R.string.export_share_button, shareOnClickListener);
+
+			bottomSheetMenu.addItem(R.string.select_all, (v) -> {
+				for (int i = 0; i < fileNames.length; i++)
+					listView.setItemChecked(i, true);
+			});
+
+			bottomSheetMenu.addItem(R.string.deselect_all, (v) -> {
+				for (int i = 0; i < fileNames.length; i++)
+					listView.setItemChecked(i, false);
+			});
+		}
 
 		setTitle(R.string.export_share_button);
 	}
@@ -84,7 +117,7 @@ public class FileSharingActivity extends DetailActivity {
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 
-		if(requestCode == SHARE_RESULT)
+		if (requestCode == SHARE_RESULT)
 			DataStore.recursiveDelete(shareableDir);
 
 		finish();
