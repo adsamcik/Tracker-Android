f968e6d334fef8dfe9703a10b79ef40d1557f00c
adsamcik <adsamcik@gmail.com>
2017-04-15 15:32:46 +0200

    Improved channelWidth tracking

--- app/src/main/java/com/adsamcik/signalcollector/data/WifiData.java
+++ app/src/main/java/com/adsamcik/signalcollector/data/WifiData.java
@@ -6,6 +6,12 @@ import android.os.Build;
 
 import java.io.Serializable;
 
+import static android.net.wifi.ScanResult.CHANNEL_WIDTH_160MHZ;
+import static android.net.wifi.ScanResult.CHANNEL_WIDTH_20MHZ;
+import static android.net.wifi.ScanResult.CHANNEL_WIDTH_40MHZ;
+import static android.net.wifi.ScanResult.CHANNEL_WIDTH_80MHZ;
+import static android.net.wifi.ScanResult.CHANNEL_WIDTH_80MHZ_PLUS_MHZ;
+
 @SuppressWarnings("unused")
 public class WifiData implements Serializable {
 	public final String BSSID;
@@ -25,8 +31,23 @@ public class WifiData implements Serializable {
 		this.capabilities = sr.capabilities;
 		if(Build.VERSION.SDK_INT > 22) {
 			this.centerFreq0 = sr.centerFreq0;
-			this.centerFreq1 = sr.centerFreq1;
-			this.channelWidth = sr.channelWidth;
+			switch (sr.channelWidth) {
+				case CHANNEL_WIDTH_20MHZ:
+					sr.channelWidth = 20;
+					break;
+				case CHANNEL_WIDTH_40MHZ:
+					sr.channelWidth = 40;
+					break;
+				case CHANNEL_WIDTH_80MHZ:
+					sr.channelWidth = 80;
+					break;
+				case CHANNEL_WIDTH_80MHZ_PLUS_MHZ:
+					this.centerFreq1 = sr.centerFreq1;
+				case CHANNEL_WIDTH_160MHZ:
+					sr.channelWidth = 160;
+					break;
+			}
+			
 			this.isPasspoint = sr.isPasspointNetwork();
 		}
 		this.frequency = sr.frequency;
