ccc90f218aee1e00d5f5a53300713c3051e48531
adsamcik <adsamcik@gmail.com>
2017-04-17 20:52:02 +0200

    Add javadoc, refactoring

--- app/src/main/java/com/adsamcik/signalcollector/data/WifiData.java
+++ app/src/main/java/com/adsamcik/signalcollector/data/WifiData.java
@@ -14,15 +14,60 @@ import static android.net.wifi.ScanResult.CHANNEL_WIDTH_80MHZ_PLUS_MHZ;
 
 @SuppressWarnings("unused")
 public class WifiData implements Serializable {
+	private static final int MAX_SIGNAL_BAR = 10;
+
+	/**
+	 * Unique wifi identification
+	 */
 	public final String BSSID;
+
+	/**
+	 * Wifi name
+	 */
 	public final String SSID;
+
+	/**
+	 * Capabilities of the network
+	 */
 	public final String capabilities;
+
+	/**
+	 * Primary frequency used to communicate with AP. Channel width is 20MHz.
+	 */
 	public final int frequency;
+
+	/**
+	 * The detected signal level in dBm.
+	 */
 	public final int level;
+
+	/**
+	 * Calculated signal level
+	 * Has value from 0 to {@value #MAX_SIGNAL_BAR}
+	 */
 	public final int bar;
+
+	/**
+	 * Center frequency, not used for 20MHz AP bandwidth
+	 * Only available on Android 5.1 and newer
+	 */
 	public int centerFreq0;
+
+	/**
+	 * Center frequency, used only for 80+80 AP bandwidth mode
+	 * Only available on Android 5.1 and newer
+	 */
 	public int centerFreq1;
+
+	/**
+	 * Channel width
+	 * Only available on Android 5.1 and newer
+	 */
 	public int channelWidth;
+
+	/**
+	 * Is wifi passpoint certified
+	 */
 	public boolean isPasspoint;
 
 	public WifiData(ScanResult sr) {
@@ -52,6 +97,6 @@ public class WifiData implements Serializable {
 		}
 		this.frequency = sr.frequency;
 		this.level = sr.level;
-		this.bar = WifiManager.calculateSignalLevel(sr.level, 10);
+		this.bar = WifiManager.calculateSignalLevel(sr.level, MAX_SIGNAL_BAR);
 	}
 }
