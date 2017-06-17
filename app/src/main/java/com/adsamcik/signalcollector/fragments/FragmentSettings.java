898ad51889807a327b8eaee4a99bcc5cf50433e5
adsamcik <adsamcik@gmail.com>
2017-06-17 16:57:43 +0200

    Hello World!

--- app/src/main/java/com/adsamcik/signalcollector/fragments/FragmentSettings.java
+++ app/src/main/java/com/adsamcik/signalcollector/fragments/FragmentSettings.java
@@ -3,6 +3,7 @@ package com.adsamcik.signalcollector.fragments;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -17,6 +18,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -359,6 +361,21 @@ public class FragmentSettings extends Fragment implements ITabFragment {
 		});
 
 		rootView.findViewById(R.id.dev_button_noise_tracking).setOnClickListener(v -> startActivity(new Intent(getActivity(), NoiseTestingActivity.class)));
+
+		rootView.findViewById(R.id.dev_button_notification_dummy).setOnClickListener(v -> {
+			String helloWorld = getString(R.string.dev_notification_dummy);
+			int color = getResources().getColor(R.color.colorPrimary);
+			NotificationCompat.Builder notiBuilder = new NotificationCompat.Builder(context, getString(R.string.channel_other_id))
+					.setSmallIcon(R.drawable.ic_signals_logo)
+					.setTicker(helloWorld)
+					.setColor(color)
+					.setLights(color, 2000, 5000)
+					.setContentTitle(helloWorld)
+					.setContentText(helloWorld)
+					.setWhen(System.currentTimeMillis());
+			NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+			notificationManager.notify(1972, notiBuilder.build());
+		});
 
 		return rootView;
 	}
