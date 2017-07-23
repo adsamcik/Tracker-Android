de8f45f342d507613ccfefcbf98543ffbb7f673d
adsamcik <adsamcik@gmail.com>
2017-07-23 16:01:13 +0200

    Add params to descriptions

--- app/src/main/java/com/adsamcik/signalcollector/data/Challenge.java
+++ app/src/main/java/com/adsamcik/signalcollector/data/Challenge.java
@@ -1,5 +1,6 @@
 package com.adsamcik.signalcollector.data;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.Resources;
 import android.support.annotation.NonNull;
@@ -8,7 +9,7 @@ import com.adsamcik.signalcollector.R;
 
 public class Challenge {
 	private final ChallengeType type;
-	public String title;
+	private String title;
 	public boolean isDone;
 	private String description;
 	private String[] descVars;
@@ -17,8 +18,8 @@ public class Challenge {
 		this.type = type;
 		this.title = title;
 		this.description = description;
-		this.isDone = isDone;
 		this.descVars = null;
+		this.isDone = isDone;
 	}
 
 	public Challenge(ChallengeType type, String title, String[] descVars, boolean isDone) {
@@ -26,9 +27,9 @@ public class Challenge {
 		this.title = title;
 		this.descVars = descVars;
 		this.description = null;
-		this.isDone = false;
+		this.isDone = isDone;
 	}
-	
+
 	public String getTitle() {
 		return title;
 	}
@@ -39,46 +40,47 @@ public class Challenge {
 
 	public void generateTexts(@NonNull Context context) {
 		Resources resources = context.getResources();
+		assert descVars != null;
 		switch (type) {
 			case LawfulExplorer:
 				title = resources.getString(R.string.challenge_lawful_explorer_title);
-				description = resources.getString(R.string.challenge_lawful_explorer_description);
+				description = resources.getString(R.string.challenge_lawful_explorer_description, descVars[0], descVars[1]);
 				break;
 			case AwfulExplorer:
 				title = resources.getString(R.string.challenge_awful_explorer_title);
-				description = resources.getString(R.string.challenge_awful_explorer_description);
+				description = resources.getString(R.string.challenge_awful_explorer_description, descVars[0]);
 				break;
 			case ScatteredData:
 				title = resources.getString(R.string.challenge_scattered_data_title);
-				description = resources.getString(R.string.challenge_scattered_data_description);
+				description = resources.getString(R.string.challenge_scattered_data_description, descVars[0]);
 				break;
 			case LongJourney:
 				title = resources.getString(R.string.challenge_long_journey_title);
-				description = resources.getString(R.string.challenge_long_journey_description);
+				description = resources.getString(R.string.challenge_long_journey_description, descVars[0]);
 				break;
 			case Crowded:
 				title = resources.getString(R.string.challenge_crowded_title);
-				description = resources.getString(R.string.challenge_crowded_description);
+				description = resources.getString(R.string.challenge_crowded_description, descVars[0]);
 				break;
 			case PeacefulForest:
 				title = resources.getString(R.string.challenge_peaceful_forest_title);
-				description = resources.getString(R.string.challenge_peaceful_forest_description);
+				description = resources.getString(R.string.challenge_peaceful_forest_description, descVars[0]);
 				break;
 			case CrossCountry:
 				title = resources.getString(R.string.challenge_cross_country_title);
-				description = resources.getString(R.string.challenge_cross_country_description);
+				description = resources.getString(R.string.challenge_cross_country_description, descVars[0]);
 				break;
 			case TripDownMemoryLane:
 				title = resources.getString(R.string.challenge_memory_lane_title);
-				description = resources.getString(R.string.challenge_memory_lane_description);
+				description = resources.getString(R.string.challenge_memory_lane_description, descVars[0]);
 				break;
 			case AIsForAlphabet:
 				title = resources.getString(R.string.challenge_alphabet_title);
-				description = resources.getString(R.string.challenge_alphabet_description);
+				description = resources.getString(R.string.challenge_alphabet_description, descVars[0], descVars[1]);
 				break;
 			case StayInRange:
 				title = resources.getString(R.string.challenge_cell_range_title);
-				description = resources.getString(R.string.challenge_cell_range_description);
+				description = resources.getString(R.string.challenge_cell_range_description, descVars[0]);
 				break;
 			default:
 				description = resources.getString(R.string.challenge_error_name);
