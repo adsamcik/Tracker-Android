376e7559e369414e95d36d271dfe4cac02fd1635
adsamcik <adsamcik@gmail.com>
2017-07-27 13:33:31 +0200

    UploadScheduler now uses Constants class

--- app/src/main/java/com/adsamcik/signalcollector/services/UploadScheduler.java
+++ app/src/main/java/com/adsamcik/signalcollector/services/UploadScheduler.java
@@ -10,16 +10,15 @@ import android.support.annotation.NonNull;
 
 import com.adsamcik.signalcollector.file.DataStore;
 import com.adsamcik.signalcollector.utility.Assist;
+import com.adsamcik.signalcollector.utility.Constants;
 import com.adsamcik.signalcollector.utility.Preferences;
 
 public class UploadScheduler extends JobService {
-
 	private static final int MIN_DELAY = Assist.MINUTE_IN_MILLISECONDS * 30;
-	private static final int MIN_SIZE_OF_DATA = 1000*1000;
 
 	public static void requestUploadSchedule(@NonNull Context context) {
 		long sizeOfData = DataStore.sizeOfData();
-		if(sizeOfData > MIN_SIZE_OF_DATA) {
+		if(sizeOfData > Constants.MIN_UPLOAD_FILE_SIZE) {
 			JobScheduler scheduler = ((JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE));
 			assert scheduler != null;
 			JobInfo.Builder jb = new JobInfo.Builder(Preferences.UPLOAD_SCHEDULE_JOB, new ComponentName(context, UploadScheduler.class));
@@ -33,7 +32,7 @@ public class UploadScheduler extends JobService {
 	@Override
 	public boolean onStartJob(JobParameters jobParameters) {
 		long sizeOfData = DataStore.sizeOfData();
-		if(sizeOfData > MIN_SIZE_OF_DATA)
+		if(sizeOfData > Constants.MIN_UPLOAD_FILE_SIZE)
 			UploadService.requestUpload(getApplicationContext(), UploadService.UploadScheduleSource.BACKGROUND);
 		return false;
 	}
