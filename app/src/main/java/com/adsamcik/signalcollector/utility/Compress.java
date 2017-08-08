928a74677dcb74d291b105715696cfb318dc100a
adsamcik <adsamcik@gmail.com>
2017-08-08 13:01:14 +0200

    Reworked Compress class

--- app/src/main/java/com/adsamcik/signalcollector/utility/Compress.java
+++ app/src/main/java/com/adsamcik/signalcollector/utility/Compress.java
@@ -9,56 +9,90 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public final class Compress {
 	private static final int BUFFER = 2048;
 
-	private Compress() {
+	private final FileOutputStream outputStream;
+	private final ZipOutputStream zipStream;
+	private final File file;
+
+	/**
+	 * Constructor for Compression class
+	 * It will keep stream active until you call finish
+	 *
+	 * @param file Zip file (will be overwritten if already exists)
+	 * @throws FileNotFoundException throws exception if there is issue with writting to zip file
+	 */
+	public Compress(File file) throws FileNotFoundException {
+		outputStream = new FileOutputStream(file);
+		zipStream = new ZipOutputStream(new BufferedOutputStream(outputStream));
+		this.file = file;
 	}
 
-	public static File zip(String path, final String[] fileNames, final String zipName) {
-		if (!path.endsWith(File.separator))
-			path += File.separatorChar;
-		try (FileOutputStream dest = new FileOutputStream(path + zipName)){
-			BufferedInputStream origin;
-
-			ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(dest));
-
-			byte data[] = new byte[BUFFER];
-
-			int fileCount = 0;
-			for (String _file : fileNames) {
-				FileInputStream fi;
-				try {
-					fi = new FileInputStream(path + _file);
-					fileCount++;
-				} catch (FileNotFoundException e) {
-					continue;
-				}
-				origin = new BufferedInputStream(fi, BUFFER);
-				ZipEntry entry = new ZipEntry(_file);
-				out.putNextEntry(entry);
-				int count;
-				while ((count = origin.read(data, 0, BUFFER)) != -1) {
-					out.write(data, 0, count);
-				}
-				origin.close();
-			}
-
-			out.close();
-			File f = new File(path + zipName);
-			if (fileCount == 0) {
-				if (!f.delete())
-					f.deleteOnExit();
-				return null;
-			}
-			return f;
-		} catch (Exception e) {
-			e.printStackTrace();
+	/**
+	 * Add file to zip from string data
+	 *
+	 * @param data data
+	 * @param name name of the file
+	 * @return true if successfully added
+	 */
+	public boolean add(final String data, final String name) {
+		ZipEntry entry = new ZipEntry(name);
+		byte[] buffer = data.getBytes();
+		try {
+			zipStream.putNextEntry(entry);
+			outputStream.write(buffer, 0, buffer.length);
+			return true;
+		} catch (IOException e) {
 			FirebaseCrash.report(e);
+			return false;
 		}
-		return null;
+	}
+
+	/**
+	 * Add file to zip
+	 *
+	 * @param file file
+	 * @return true if successfully added
+	 */
+	public boolean add(File file) {
+		FileInputStream fi;
+
+		try {
+			fi = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			FirebaseCrash.report(e);
+			return false;
+		}
+		BufferedInputStream origin = new BufferedInputStream(fi, BUFFER);
+		ZipEntry entry = new ZipEntry(file.getName());
+		byte data[] = new byte[BUFFER];
+		try {
+			zipStream.putNextEntry(entry);
+			int count;
+			while ((count = origin.read(data, 0, BUFFER)) != -1)
+				outputStream.write(data, 0, count);
+
+			origin.close();
+			return true;
+		} catch (IOException e) {
+			FirebaseCrash.report(e);
+			return false;
+		}
+	}
+
+	/**
+	 * Call after you're done writting into the zip file
+	 *
+	 * @return zip file
+	 * @throws IOException Exception is thrown if error occurred during stream closing
+	 */
+	public File finish() throws IOException {
+		outputStream.close();
+		return file;
 	}
 }
