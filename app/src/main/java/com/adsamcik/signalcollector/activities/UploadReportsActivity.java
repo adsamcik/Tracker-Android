07c6c53c4fd897f9038a185366f541d7b4575a51
adsamcik <adsamcik@gmail.com>
2017-08-11 09:43:59 +0200

    Reversed order of upload reports

--- app/src/main/java/com/adsamcik/signalcollector/activities/UploadReportsActivity.java
+++ app/src/main/java/com/adsamcik/signalcollector/activities/UploadReportsActivity.java
@@ -70,7 +70,7 @@ public class UploadReportsActivity extends DetailActivity {
 		setTitle(R.string.recent_uploads);
 
 		UploadStats[] recent = new Gson().fromJson(DataStore.loadAppendableJsonArray(this, DataStore.RECENT_UPLOADS_FILE), UploadStats[].class);
-		Arrays.sort(recent, (uploadStats, t1) -> (int) ((uploadStats.time - t1.time) / MINUTE_IN_MILLISECONDS));
+		Arrays.sort(recent, (uploadStats, t1) -> (int) ((t1.time - uploadStats.time) / MINUTE_IN_MILLISECONDS));
 		if (recent.length > 0) {
 			Context context = getApplicationContext();
 			LinearLayout parent = createContentParent(false);
