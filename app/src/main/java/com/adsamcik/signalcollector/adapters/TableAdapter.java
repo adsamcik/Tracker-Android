99d5bf49bebe622f4736807a43ade5574de2b7c5
adsamcik <adsamcik@gmail.com>
2017-09-05 14:06:49 +0200

    Add TableAdapter view reusing

--- app/src/main/java/com/adsamcik/signalcollector/adapters/TableAdapter.java
+++ app/src/main/java/com/adsamcik/signalcollector/adapters/TableAdapter.java
@@ -3,6 +3,7 @@ package com.adsamcik.signalcollector.adapters;
 import android.content.Context;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -69,6 +70,8 @@ public class TableAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(int i, View view, ViewGroup viewGroup) {
+		if(view != null && view instanceof FrameLayout)
+			return view;
 		ViewGroup v = (ViewGroup) tables.get(i).getView(context, true);
 
 		FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) v.getChildAt(0).getLayoutParams();
