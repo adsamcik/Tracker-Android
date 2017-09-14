ecd1af63992a47cc39a054af4a71d4a4e7dd9332
adsamcik <adsamcik@gmail.com>
2017-09-14 10:40:33 +0200

    Add javadoc

--- app/src/main/java/com/adsamcik/signalcollector/interfaces/IFilterRule.java
+++ app/src/main/java/com/adsamcik/signalcollector/interfaces/IFilterRule.java
@@ -3,5 +3,13 @@ package com.adsamcik.signalcollector.interfaces;
 import android.support.annotation.NonNull;
 
 public interface IFilterRule<T> {
+	/**
+	 * Filter function that returns true if item should be displayed
+	 *
+	 * @param value Value to filter
+	 * @param stringValue Value as string to filter
+	 * @param constraint Constraint string
+	 * @return true if value should be shown
+	 */
 	boolean filter(@NonNull T value, @NonNull String stringValue, @NonNull CharSequence constraint);
 }
--- app/src/main/java/com/adsamcik/signalcollector/interfaces/IString.java
+++ app/src/main/java/com/adsamcik/signalcollector/interfaces/IString.java
@@ -3,5 +3,13 @@ package com.adsamcik.signalcollector.interfaces;
 import android.support.annotation.NonNull;
 
 public interface IString<T> {
-	@NonNull String stringify(@NonNull T item);
+	/**
+	 * Method that converts objects to string
+	 * This allows for custom tostring implementations that can be passed as parameters
+	 *
+	 * @param item item that will be converted to string
+	 * @return supplied item as string
+	 */
+	@NonNull
+	String stringify(@NonNull T item);
 }
