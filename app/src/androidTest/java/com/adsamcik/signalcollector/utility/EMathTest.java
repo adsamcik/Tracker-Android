5207cbab572de709e5f327deb94455386ef4a731
adsamcik <adsamcik@gmail.com>
2017-10-08 12:40:13 +0200

    Fix step function

--- app/src/androidTest/java/com/adsamcik/signalcollector/utility/EMathTest.java
+++ app/src/androidTest/java/com/adsamcik/signalcollector/utility/EMathTest.java
@@ -8,8 +8,17 @@ import static org.junit.Assert.*;
 public class EMathTest {
 	@Test
 	public void step() throws Exception {
-		Assert.assertEquals(150, EMath.step(50, 90, 15));
-		Assert.assertEquals(30, EMath.step(0, 28, 30));
+		Assert.assertEquals(30, EMath.step(0, 30, 30));
+		Assert.assertEquals(30, EMath.step(0, 1, 30));
+		Assert.assertEquals(60, EMath.step(90, -1, 30));
+
+		//negative value test
+		Assert.assertEquals(-60, EMath.step(-90, 1, 30));
+		Assert.assertEquals(-60, EMath.step(-30, -1, 30));
+
+		//0 direction test
+		Assert.assertEquals(-30, EMath.step(-30, 0, 30));
+		Assert.assertEquals(30, EMath.step(30, 0, 30));
 	}
 
 }
--- app/src/main/java/com/adsamcik/signalcollector/utility/EMath.java
+++ app/src/main/java/com/adsamcik/signalcollector/utility/EMath.java
@@ -2,14 +2,19 @@ package com.adsamcik.signalcollector.utility;
 
 public class EMath {
 	public static int step(int value, int direction, final int step) {
-		int increment = Math.abs(direction) / step * sign(direction);
-		return value + increment * step;
+		int target = value + direction;
+
+		if (target % step != 0 && sign(direction) == sign(target))
+			target = target / step + sign(direction);
+		else
+			target /= step;
+		return target * step;
 	}
 
 	public static int sign(int value) {
-		if(value > 0)
+		if (value > 0)
 			return 1;
-		else if(value < 0)
+		else if (value < 0)
 			return -1;
 		else
 			return 0;
