dfdb0b6b47e7889e52c5bbb1c371710c33e7c49b
adsamcik <adsamcik@gmail.com>
2017-11-13 12:30:44 +0100

    Add constructors

--- app/src/main/java/com/adsamcik/signalcollector/utility/TranslateableString.java
+++ app/src/main/java/com/adsamcik/signalcollector/utility/TranslateableString.java
@@ -2,14 +2,25 @@ package com.adsamcik.signalcollector.utility;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.google.firebase.crash.FirebaseCrash;
 import com.vimeo.stag.UseStag;
 
 @UseStag
 public abstract class TranslateableString {
-	private String defaultString = null;
-	private String identifier = null;
+	private String defaultString;
+	private String identifier;
+
+	public TranslateableString() {
+		defaultString = null;
+		identifier = null;
+	}
+
+	public TranslateableString(@NonNull String identifier, @Nullable String defaultString) {
+		this.identifier = identifier;
+		this.defaultString = defaultString;
+	}
 
 	public String getString(@NonNull Context context) {
 		if (identifier == null)
