d2a0eceacc3567efdfd9835e3ceb5e68a3b00c25
adsamcik <adsamcik@users.noreply.github.com>
2017-11-17 19:23:08 +0100

    Updated interface definition

--- app/src/main/java/com/adsamcik/signalcollector/signin/ISignInClient.java
+++ app/src/main/java/com/adsamcik/signalcollector/signin/ISignInClient.java
@@ -2,15 +2,16 @@ package com.adsamcik.signalcollector.signin;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+
+import com.adsamcik.signalcollector.interfaces.IValueCallback;
 
 interface ISignInClient {
 	int RC_SIGN_IN = 4654;
 
-	void signIn(@NonNull final Activity activity);
-	void signInSilent(@NonNull final Context context);
-
-	void signOut(@NonNull final Activity activity);
-
-
+	void signIn(@NonNull final Activity activity, @NonNull IValueCallback<User> userValueCallback);
+	void signInSilent(@NonNull final Context context, @NonNull IValueCallback<User> userValueCallback);
+	void signOut(@NonNull final Context context);
+	void onSignInResult(@NonNull Activity activity,int resultCode, Intent data);
 }
