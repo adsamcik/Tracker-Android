078731b17c72d123f0cb3ad9a5b0d1402a831138
adsamcik <adsamcik@users.noreply.github.com>
2017-11-20 19:15:47 +0100

    Updated DataStoreTest to Kotlin

---
+++ app/src/androidTest/java/com/adsamcik/signalcollector/file/DataStoreTest.kt
@@ -0,0 +1,49 @@
+package com.adsamcik.signalcollector.file
+
+import android.os.Build
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import android.util.Log
+import com.adsamcik.signalcollector.BuildConfig
+import com.adsamcik.signalcollector.data.RawData
+import com.adsamcik.signalcollector.signin.Signin
+import com.adsamcik.signalcollector.utility.Constants
+import com.google.gson.Gson
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class DataStoreTest {
+    private val gson = Gson()
+    private val appContext = InstrumentationRegistry.getTargetContext()
+
+    @Before
+    fun clearAll() {
+        DataStore.clearAll(appContext)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun saveArraySigned() {
+        val fileHeader = "\"model\":\"" + Build.MODEL +
+                "\",\"manufacturer\":\"" + Build.MANUFACTURER +
+                "\",\"api\":" + Build.VERSION.SDK_INT +
+                ",\"version\":" + BuildConfig.VERSION_CODE + "," +
+                "\"data\":"
+
+        if (!Signin.isSignedIn) {
+            Log.w("SignalsTest", "Please sign in before doing this test")
+            return
+        }
+
+        val rawData = arrayOf(RawData(System.currentTimeMillis()),
+                RawData(System.currentTimeMillis() + Constants.MINUTE_IN_MILLISECONDS))
+
+        assertEquals(DataStore.SaveStatus.SAVE_SUCCESS, DataStore.saveData(appContext, rawData))
+        val loadedData = DataStore.loadAppendableJsonArray(appContext, DataStore.DATA_FILE + 0 + DataFile.SEPARATOR + 2)
+        val firstComma = loadedData!!.indexOf(',')
+        assertEquals(fileHeader + gson.toJson(rawData), loadedData.substring(firstComma + 1))
+    }
+}
