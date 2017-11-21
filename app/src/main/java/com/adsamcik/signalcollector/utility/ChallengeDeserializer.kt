c15e747b1d504917734959376a39b621931fd5ac
adsamcik <adsamcik@users.noreply.github.com>
2017-11-21 14:01:56 +0100

    Updated ChallengeDeserializer to Kotlin

---
+++ app/src/main/java/com/adsamcik/signalcollector/utility/ChallengeDeserializer.kt
@@ -0,0 +1,31 @@
+package com.adsamcik.signalcollector.utility
+
+import com.adsamcik.signalcollector.data.Challenge
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonParseException
+import java.lang.reflect.Type
+
+class ChallengeDeserializer : JsonDeserializer<Challenge> {
+    @Throws(JsonParseException::class)
+    override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): Challenge {
+        val jobject = json.asJsonObject
+        val jDescVars = jobject.get("descVars").asJsonArray
+        val descVars = arrayOfNulls<String>(jDescVars.size())
+
+        for (i in descVars.indices)
+            descVars[i] = jDescVars.get(i).asString
+
+        val progressElement = jobject.get("progress")
+        val progress: Float
+        progress = progressElement?.asFloat ?: 0f
+
+        return Challenge(
+                Challenge.ChallengeType.valueOf(jobject.get("type").asString),
+                jobject.get("title").asString,
+                descVars,
+                progress,
+                jobject.get("difficulty").asInt)
+    }
+}
