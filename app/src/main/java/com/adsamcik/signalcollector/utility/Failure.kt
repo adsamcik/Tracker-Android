e6cfdadf765780738e2e154f6b5b57c2acffd785
adsamcik <adsamcik@users.noreply.github.com>
2017-11-21 14:03:57 +0100

    Updated Failure to Kotlin

---
+++ app/src/main/java/com/adsamcik/signalcollector/utility/Failure.kt
@@ -0,0 +1,28 @@
+package com.adsamcik.signalcollector.utility
+
+class Failure<T> {
+    val value: T?
+
+    /**
+     * Creates successful instance
+     */
+    constructor() {
+        value = null
+    }
+
+    /**
+     * Create unsuccessful instance
+     *
+     * @param message value
+     */
+    constructor(message: T) {
+        this.value = message
+    }
+
+    /**
+     * Returns failure
+     *
+     * @return true if failed
+     */
+    fun hasFailed(): Boolean = value != null
+}
