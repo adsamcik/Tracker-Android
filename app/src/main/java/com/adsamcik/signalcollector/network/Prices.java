67f2480c17b4483a2db7dd4721b09a8ef0c8b62b
adsamcik <adsamcik@users.noreply.github.com>
2017-11-25 13:30:19 +0100

    Updated Prices to Kotlin

--- app/src/main/java/com/adsamcik/signalcollector/network/Prices.java
+++
@@ -1,6 +0,0 @@
-package com.adsamcik.signalcollector.network;
-
-public class Prices {
-	public int PRICE_30DAY_MAP;
-	public int PRICE_30DAY_PERSONAL_MAP;
-}
---
+++ app/src/main/java/com/adsamcik/signalcollector/network/Prices.kt
@@ -0,0 +1,7 @@
+package com.adsamcik.signalcollector.network
+
+
+class Prices {
+    var PRICE_30DAY_MAP: Int = 0
+    var PRICE_30DAY_PERSONAL_MAP: Int = 0
+}
