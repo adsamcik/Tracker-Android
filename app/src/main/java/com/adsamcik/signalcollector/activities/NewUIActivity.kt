86cd2ca3b5c22d0e00f5df7ce736b07caf22b5cb
adsamcik <adsamcik@users.noreply.github.com>
2017-12-18 18:25:00 +0100

    Add WIP new main activity

---
+++ app/src/main/java/com/adsamcik/signalcollector/activities/NewUIActivity.kt
@@ -0,0 +1,14 @@
+package com.adsamcik.signalcollector.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.adsamcik.signalcollector.R
+
+class NewUIActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_ui)
+    }
+
+}
