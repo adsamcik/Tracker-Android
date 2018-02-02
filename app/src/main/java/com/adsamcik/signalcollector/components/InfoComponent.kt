a7b8ef06b24050e5c5fc8ca6eff6ce8f06078079
adsamcik <adsamcik@users.noreply.github.com>
2018-02-02 13:19:08 +0100

    Add import

--- app/src/main/java/com/adsamcik/signalcollector/components/InfoComponent.kt
+++ app/src/main/java/com/adsamcik/signalcollector/components/InfoComponent.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
+import com.adsamcik.signalcollector.R
 import com.adsamcik.utilities.Assist
 
 
