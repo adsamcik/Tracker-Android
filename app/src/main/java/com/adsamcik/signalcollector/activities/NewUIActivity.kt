121e9fc3705bc0fd4d52fad2e60e3d72ec7ff08f
adsamcik <adsamcik@users.noreply.github.com>
2018-02-07 16:36:44 +0100

    Updated NewUI layout

--- app/src/main/java/com/adsamcik/signalcollector/activities/NewUIActivity.kt
+++ app/src/main/java/com/adsamcik/signalcollector/activities/NewUIActivity.kt
@@ -17,18 +17,33 @@ class NewUIActivity : AppCompatActivity() {
         val root = findViewById<CoordinatorLayout>(R.id.root)
 
         val animation = ValueAnimator.ofArgb(Color.parseColor("#166f72"), Color.parseColor("#2e4482"))
-        animation.duration = 5000
+        animation.duration = 50000
         animation.repeatMode = ValueAnimator.REVERSE
         animation.repeatCount = ValueAnimator.INFINITE
         animation.addUpdateListener { root.setBackgroundColor(it.animatedValue as Int) }
         animation.start()
 
         val wifiComponent = findViewById<InfoComponent>(R.id.tracker_wifi_component)
-        wifiComponent.addSecondaryText("found them 5meters ago")
-        wifiComponent.addPrimaryText("56 wifi nearby")
+        wifiComponent.addSecondaryText("found 6 meters before collection")
+        wifiComponent.addPrimaryText("In range of 150 Wifi's")
+        wifiComponent.addPrimaryText("In range of 150 Wifi's")
+        wifiComponent.addPrimaryText("In range of 150 Wifi's")
+        wifiComponent.addPrimaryText("In range of 150 Wifi's")
+        wifiComponent.addPrimaryText("In range of 150 Wifi's")
+        wifiComponent.addPrimaryText("In range of 150 Wifi's")
 
         val cellComponent = findViewById<InfoComponent>(R.id.tracker_cell_component)
-        cellComponent.addPrimaryText("Healthy LTE, you fed it well")
+        cellComponent.addPrimaryText("LTE -89 dbm, 51 asu")
+        cellComponent.addPrimaryText("In range of 12 base stations")
+
+        val demo2Component = findViewById<InfoComponent>(R.id.tracker_demo2_component)
+        demo2Component.addSecondaryText("found 6 meters before collection")
+        demo2Component.addPrimaryText("In range of 150 Wifi's")
+        demo2Component.addPrimaryText("In range of 12 base stations")
+
+        val demoComponent = findViewById<InfoComponent>(R.id.tracker_demo_component)
+        demoComponent.addPrimaryText("LTE -89 dbm, 51 asu")
+        demoComponent.addPrimaryText("In range of 12 base stations")
     }
 
 
