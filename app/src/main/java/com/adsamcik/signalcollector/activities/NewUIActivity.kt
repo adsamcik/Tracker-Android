2be0eb86993e3a5ed76a9f15534200d1d4866e07
adsamcik <adsamcik@users.noreply.github.com>
2018-02-10 09:30:22 +0100

    Updated ids to better match code style and binding

--- app/src/main/java/com/adsamcik/signalcollector/activities/NewUIActivity.kt
+++ app/src/main/java/com/adsamcik/signalcollector/activities/NewUIActivity.kt
@@ -5,10 +5,9 @@ import android.support.design.widget.CoordinatorLayout
 import android.support.v7.app.AppCompatActivity
 import com.adsamcik.draggable.DragAxis
 import com.adsamcik.draggable.DragTargetAnchor
-import com.adsamcik.draggable.DraggableImageButton
 import com.adsamcik.signalcollector.R
-import com.adsamcik.signalcollector.components.InfoComponent
 import com.adsamcik.signalcollector.uitools.ColorManager
+import kotlinx.android.synthetic.main.activity_new_ui.*
 
 
 class NewUIActivity : AppCompatActivity() {
@@ -17,7 +16,6 @@ class NewUIActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_ui)
-        val root = findViewById<CoordinatorLayout>(R.id.root)
 
         colorManager = ColorManager(root, this)
         val colorManager = colorManager!!
@@ -25,37 +23,30 @@ class NewUIActivity : AppCompatActivity() {
         //colorManager.addColors(Color.parseColor("#166f72"), Color.parseColor("#2e4482"), Color.parseColor("#ffc100"), Color.parseColor("#fff400"))
         //colorManager.addColors(Color.parseColor("#cccccc"), Color.parseColor("#2e4482"), Color.parseColor("#ffc100"), Color.parseColor("#fff400"))
 
-        val wifiComponent = findViewById<InfoComponent>(R.id.tracker_wifi_component)
-        wifiComponent.addSecondaryText("found 6 meters before collection")
-        wifiComponent.addPrimaryText("In range of 150 Wifi's")
-        wifiComponent.addPrimaryText("In range of 150 Wifi's")
-        wifiComponent.addPrimaryText("In range of 150 Wifi's")
-        wifiComponent.addPrimaryText("In range of 150 Wifi's")
-        wifiComponent.addPrimaryText("In range of 150 Wifi's")
-        wifiComponent.addPrimaryText("In range of 150 Wifi's")
+        trackerWifiComponent.addSecondaryText("found 6 meters before collection")
+        trackerWifiComponent.addPrimaryText("In range of 150 Wifi's")
+        trackerWifiComponent.addPrimaryText("In range of 150 Wifi's")
+        trackerWifiComponent.addPrimaryText("In range of 150 Wifi's")
+        trackerWifiComponent.addPrimaryText("In range of 150 Wifi's")
+        trackerWifiComponent.addPrimaryText("In range of 150 Wifi's")
+        trackerWifiComponent.addPrimaryText("In range of 150 Wifi's")
 
-        val cellComponent = findViewById<InfoComponent>(R.id.tracker_cell_component)
-        cellComponent.addPrimaryText("LTE -89 dbm, 51 asu")
-        cellComponent.addPrimaryText("In range of 12 base stations")
+        trackerCellComponent.addPrimaryText("LTE -89 dbm, 51 asu")
+        trackerCellComponent.addPrimaryText("In range of 12 base stations")
 
-        val demo2Component = findViewById<InfoComponent>(R.id.tracker_demo2_component)
-        demo2Component.addSecondaryText("found 6 meters before collection")
-        demo2Component.addPrimaryText("In range of 150 Wifi's")
-        demo2Component.addPrimaryText("In range of 12 base stations")
+        trackerDemo2Component.addSecondaryText("found 6 meters before collection")
+        trackerDemo2Component.addPrimaryText("In range of 150 Wifi's")
+        trackerDemo2Component.addPrimaryText("In range of 12 base stations")
 
-        val demoComponent = findViewById<InfoComponent>(R.id.tracker_demo_component)
-        demoComponent.addPrimaryText("LTE -89 dbm, 51 asu")
-        demoComponent.addPrimaryText("In range of 12 base stations")
+        trackerDemoComponent.addPrimaryText("LTE -89 dbm, 51 asu")
+        trackerDemoComponent.addPrimaryText("In range of 12 base stations")
 
-        val statsButton = findViewById<DraggableImageButton>(R.id.stats_button)
         statsButton.setDrag(DragAxis.X)
         statsButton.setTarget(root, DragTargetAnchor.TopRight, -10)
 
-        val activityButton = findViewById<DraggableImageButton>(R.id.activity_button)
         activityButton.setDrag(DragAxis.X)
         activityButton.setTarget(root, DragTargetAnchor.TopLeft, -10)
 
-        val mapDraggable = findViewById<DraggableImageButton>(R.id.map_draggable)
         mapDraggable.setDrag(DragAxis.Y)
         mapDraggable.setTarget(root, DragTargetAnchor.Top, 64)
 
