c5fce0b0a6553e0095bee14b66fcac412255b945
adsamcik <adsamcik@users.noreply.github.com>
2018-02-17 13:18:30 +0100

    Refactoring and add persistence

--- app/src/main/java/com/adsamcik/signalcollector/components/ImageSwitchPreference.kt
+++ app/src/main/java/com/adsamcik/signalcollector/components/ImageSwitchPreference.kt
@@ -7,6 +7,7 @@ import android.support.annotation.DrawableRes
 import android.support.v7.preference.Preference
 import android.support.v7.preference.PreferenceViewHolder
 import android.util.AttributeSet
+import android.util.Log
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -17,6 +18,18 @@ import com.adsamcik.signalcollector.utility.Assist
 
 
 class ImageSwitchPreference : Preference {
+    private var mForegroundColors: ColorStateList? = null
+
+    private var mItems = ArrayList<SwitchItem>()
+    private var mTextView: TextView? = null
+    private var mImageRoot: ViewGroup? = null
+
+    private var mInitialized = false
+    private val mImageSizePx = Assist.dpToPx(context, 50)
+    private val mMarginPx = Assist.dpToPx(context, 10)
+
+    private var mSelected: Int = -1
+
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes) {
         initAttributes(context, attrs!!)
     }
@@ -37,7 +50,7 @@ class ImageSwitchPreference : Preference {
         val titlesResource = attributes.getResourceId(R.styleable.ImageSwitchPreference_titles, 0)
         val drawablesResource = attributes.getResourceId(R.styleable.ImageSwitchPreference_drawables, 0)
 
-        foregroundColors = attributes.getColorStateList(R.styleable.ImageSwitchPreference_foregroundStateColors) ?: ColorStateList(arrayOf(intArrayOf(android.R.attr.state_enabled), intArrayOf(android.R.attr.state_selected)), intArrayOf(Color.GRAY, Color.BLUE))
+        mForegroundColors = attributes.getColorStateList(R.styleable.ImageSwitchPreference_foregroundStateColors) ?: ColorStateList(arrayOf(intArrayOf(android.R.attr.state_enabled), intArrayOf(android.R.attr.state_selected)), intArrayOf(Color.GRAY, Color.BLUE))
 
         attributes.recycle()
 
@@ -56,64 +69,68 @@ class ImageSwitchPreference : Preference {
         drawables.recycle()
     }
 
-    private var foregroundColors: ColorStateList? = null
-
-    private var items = ArrayList<SwitchItem>()
-    private var textView: TextView? = null
-    private var imageRoot: ViewGroup? = null
-
-    private var initialized = false
-    private val imageSizePx = Assist.dpToPx(context, 50)
-    private val marginPx = Assist.dpToPx(context, 10)
-
-    private var selected: Int = -1
-
     init {
         layoutResource = R.layout.layout_image_switch
     }
 
     fun addItem(title: String, @DrawableRes image: Int) {
         val item = SwitchItem(title, image)
-        items.add(item)
+        mItems.add(item)
 
-        if (initialized)
-            initializeItem(item, items.lastIndex)
+        if (mInitialized)
+            initializeItem(item, mItems.lastIndex)
     }
 
     private fun onClick(index: Int) {
-        if (selected >= 0) {
-            items[selected].imageView!!.isSelected = false
-        }
-        val newItem = items[index]
-        newItem.imageView!!.isSelected = true
-        textView!!.text = newItem.title
 
-        selected = index
+
+        select(index)
+    }
+
+    private fun select(index: Int) {
+        when {
+            mSelected == index -> return
+            index < 0 -> return
+            mSelected >= 0 -> mItems[mSelected].imageView!!.isSelected = false
+        }
+
+        val newItem = mItems[index]
+        newItem.imageView!!.isSelected = true
+        mTextView!!.text = newItem.title
+
+        mSelected = index
+        if (!persistInt(index)) {
+            Log.w("Signals", "persistInt() returned false. Check if this preference has a key.")
+            return
+        }
     }
 
     private fun initializeItem(item: SwitchItem, index: Int) {
         val view = ImageView(context)
         view.contentDescription = item.title
         view.setImageResource(item.drawable)
-        view.imageTintList = foregroundColors
-        val layoutParams = LinearLayout.LayoutParams(imageSizePx, imageSizePx)
-        layoutParams.setMargins(marginPx)
+        view.imageTintList = mForegroundColors
+        val layoutParams = LinearLayout.LayoutParams(mImageSizePx, mImageSizePx)
+        layoutParams.setMargins(mMarginPx)
         view.layoutParams = layoutParams
         view.setOnClickListener { onClick(index) }
 
-        imageRoot!!.addView(view)
+        mImageRoot!!.addView(view)
         item.imageView = view
     }
 
     override fun onBindViewHolder(holder: PreferenceViewHolder) {
         super.onBindViewHolder(holder)
         holder.itemView.isClickable = false
-        textView = holder.findViewById(R.id.title) as TextView
-        imageRoot = holder.findViewById(R.id.option_root) as ViewGroup
-        initialized = true
-        items.forEachIndexed { index, switchItem ->
+        mTextView = holder.findViewById(R.id.title) as TextView
+        mImageRoot = holder.findViewById(R.id.option_root) as ViewGroup
+        mItems.forEachIndexed { index, switchItem ->
             initializeItem(switchItem, index)
         }
+
+        select(getPersistedInt(-1))
+
+        mInitialized = true
     }
 
     class SwitchItem(val title: String, @DrawableRes val drawable: Int, var imageView: ImageView? = null)
