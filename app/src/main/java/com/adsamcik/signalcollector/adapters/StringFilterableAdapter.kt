2e3d97480836d5c2b1359d7bc5da0d3a4d57f563
adsamcik <adsamcik@users.noreply.github.com>
2018-03-30 12:51:13 +0200

    Fixed filtering in StringFilterableAdapter

--- app/src/main/java/com/adsamcik/signalcollector/adapters/StringFilterableAdapter.kt
+++ app/src/main/java/com/adsamcik/signalcollector/adapters/StringFilterableAdapter.kt
@@ -6,6 +6,6 @@ import android.support.annotation.LayoutRes
 
 class StringFilterableAdapter(context: Context, @LayoutRes resource: Int, stringMethod: (Array<String>) -> String) : FilterableAdapter<Array<String>, String>(context, resource, stringMethod) {
     override fun filter(item: Array<String>, filterObject: String?): Boolean {
-        return true
+        return filterObject == null || item.contains(filterObject)
     }
 }
