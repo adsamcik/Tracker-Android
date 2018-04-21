cce2c73be0d6c26004c90874135b768819b5260e
adsamcik <adsamcik@users.noreply.github.com>
2018-04-21 13:42:22 +0200

    Add javadoc

--- app/src/main/java/com/adsamcik/signalcollector/extensions/LiveDataExtensions.kt
+++ app/src/main/java/com/adsamcik/signalcollector/extensions/LiveDataExtensions.kt
@@ -4,6 +4,13 @@ import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.Observer
 
+/**
+ * LiveData observe that allows Higher order function instead of observer
+ * More at [LiveData.observe]
+ *
+ * @param owner Lifecycle owner
+ * @param body Higher order function that is executed when live data changes
+ */
 fun <T> LiveData<T>.observe(owner: LifecycleOwner, body: (T?) -> Unit) {
     observe(owner, Observer<T> { t -> body(t) })
 }
