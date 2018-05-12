d16415fcc93e35ba84bf2003fdb663978e0c7d94
adsamcik <adsamcik@users.noreply.github.com>
2018-05-12 12:42:38 +0200

    Add ExtensionTests

---
+++ app/src/androidTest/java/com/adsamcik/signalcollector/utility/ExtensionTests.kt
@@ -0,0 +1,34 @@
+package com.adsamcik.signalcollector.utility
+
+import com.adsamcik.signalcollector.extensions.contains
+import com.adsamcik.signalcollector.extensions.date
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import java.util.*
+
+@RunWith(JUnit4::class)
+class ExtensionTests {
+    @Test
+    fun listContainsTest() {
+        val list = listOf("x", "xar", "y", "d")
+
+        assertTrue(list.contains { it == "xar" })
+        assertFalse(list.contains { it == "xa" })
+    }
+
+    @Test
+    fun roundToDateTest() {
+        val cal = Calendar.getInstance()
+        val calRound = cal.date()
+
+        assertEquals(cal[Calendar.DAY_OF_MONTH], calRound[Calendar.DAY_OF_MONTH])
+        assertEquals(cal[Calendar.MONTH], calRound[Calendar.MONTH])
+        assertEquals(cal[Calendar.YEAR], calRound[Calendar.YEAR])
+        assertEquals(0, calRound[Calendar.HOUR_OF_DAY])
+        assertEquals(0, calRound[Calendar.MINUTE])
+        assertEquals(0, calRound[Calendar.SECOND])
+        assertEquals(0, calRound[Calendar.MILLISECOND])
+    }
+}
