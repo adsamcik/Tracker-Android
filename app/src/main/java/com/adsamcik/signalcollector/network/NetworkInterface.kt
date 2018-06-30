f78f3a622001172eed1977f6a445f56bd69c13b7
adsamcik <adsamcik@users.noreply.github.com>
2018-06-30 11:01:44 +0200

    Removed unused consts

--- app/src/main/java/com/adsamcik/signalcollector/network/NetworkInterface.kt
+++ app/src/main/java/com/adsamcik/signalcollector/network/NetworkInterface.kt
@@ -5,25 +5,6 @@ import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.*
 
-
-const val URL_AUTHENTICATE = Server.URL_AUTHENTICATE
-const val URL_DATA_UPLOAD = Server.URL_DATA_UPLOAD
-const val URL_TILES = Server.URL_TILES
-const val URL_PERSONAL_TILES = Server.URL_PERSONAL_TILES
-const val URL_USER_STATS = Server.URL_USER_STATS
-const val URL_STATS = Server.URL_STATS
-const val URL_GENERAL_STATS = Server.URL_GENERAL_STATS
-const val URL_MAPS_AVAILABLE = Server.URL_MAPS_AVAILABLE
-const val URL_FEEDBACK = Server.URL_FEEDBACK
-const val URL_USER_INFO = Server.URL_USER_INFO
-const val URL_USER_PRICES = Server.URL_USER_PRICES
-const val URL_CHALLENGES_LIST = Server.URL_CHALLENGES_LIST
-const val URL_PRIVACY_POLICY = Server.URL_PRIVACY_POLICY
-
-const val URL_USER_UPDATE_MAP_PREFERENCE = Server.URL_USER_UPDATE_MAP_PREFERENCE
-const val URL_USER_UPDATE_PERSONAL_MAP_PREFERENCE = Server.URL_USER_UPDATE_PERSONAL_MAP_PREFERENCE
-
-
 interface NetworkInterface {
     @POST(Network.URL_AUTHENTICATE)
     fun authenticate(@Query("token") token: String): Call<JwtData>
