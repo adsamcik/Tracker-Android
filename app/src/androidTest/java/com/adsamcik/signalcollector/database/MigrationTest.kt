ce1f66b474cfc326785f4235ba3320d87354c218
adsamcik <adsamcik@users.noreply.github.com>
2019-03-28 12:03:22 +0100

    Updated migration script

--- app/src/androidTest/java/com/adsamcik/signalcollector/database/MigrationTest.kt
+++ app/src/androidTest/java/com/adsamcik/signalcollector/database/MigrationTest.kt
@@ -1,5 +1,7 @@
 package com.adsamcik.signalcollector.database
 
+import androidx.core.database.getDoubleOrNull
+import androidx.core.database.getFloatOrNull
 import androidx.room.testing.MigrationTestHelper
 import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -34,7 +36,7 @@ class MigrationTest {
 		db.execSQL("insert into location_data (id, time, lat, lon, alt, hor_acc, activity, confidence) values (1, '1552026583', '27.30459', '68.39764', -36.2, 26.6, 3, 38)")
 		db.execSQL("insert into location_data (id, time, lat, lon, alt, hor_acc, activity, confidence) values (2, '1522659716', 52.5094874, 16.7474972, -97.4, 67.7, 3, 69)")
 		db.execSQL("insert into location_data (id, time, lat, lon, alt, hor_acc, activity, confidence) values (3, '1528243933', 19.1780491, -96.1288426, -34.0, 31.3, 3, 6)")
-		db.execSQL("insert into location_data (id, time, lat, lon, alt, hor_acc, activity, confidence) values (4, '1544682291', 50.7036309, 18.995304, 39.2, 4.6, 3, 64)")
+		db.execSQL("insert into location_data (id, time, lat, lon, alt, hor_acc, activity, confidence) values (4, '1544682291', 50.7036309, 18.995304, 0.0, 0.0, 3, 64)")
 		db.execSQL("insert into location_data (id, time, lat, lon, alt, hor_acc, activity, confidence) values (5, '1529254903', 53.5830905, 9.7537598, 45.7, 73.3, 3, 86)")
 		// Prepare for the next version.
 		db.close()
@@ -42,8 +44,8 @@ class MigrationTest {
 
 		// Re-open the database with version 3 and provide
 		// MIGRATION_2_3 as the migration process.
-		helper.runMigrationsAndValidate(TEST_DB, 2, true, MIGRATION_2_3).apply {
-			val cursor = query("SELECT * FROM location_data WHERE id == 3")
+		helper.runMigrationsAndValidate(TEST_DB, 3, true, MIGRATION_2_3).apply {
+			var cursor = query("SELECT * FROM location_data WHERE id == 3")
 
 			with(cursor) {
 				val hasNext = moveToNext()
@@ -53,10 +55,19 @@ class MigrationTest {
 				assertEquals(19.1780491, getDouble(2), 0.00001)
 				assertEquals(-96.1288426, getDouble(3), 0.00001)
 				assertEquals(-34.0, getDouble(4), 0.00001)
-				assertEquals(31.3, getDouble(5), 0.00001)
-				assertEquals(3, getInt(6))
-				assertEquals(6, getInt(7))
+				assertEquals(31.3f, getFloat(5), 0.00001f)
+				assertEquals(3, getInt(7))
+				assertEquals(6, getInt(8))
+			}
 
+			cursor = query("SELECT * FROM location_data WHERE id == 4")
+
+			with(cursor) {
+				val hasNext = moveToNext()
+				assertTrue(hasNext)
+				assertEquals(4, getInt(0))
+				assertEquals(null, getDoubleOrNull(4))
+				assertEquals(null, getFloatOrNull(5))
 			}
 
 		}
--- app/src/main/java/com/adsamcik/signalcollector/database/AppDatabaseMigrations.kt
+++ app/src/main/java/com/adsamcik/signalcollector/database/AppDatabaseMigrations.kt
@@ -5,9 +5,17 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 
 val MIGRATION_2_3 = object : Migration(2, 3) {
 	override fun migrate(database: SupportSQLiteDatabase) {
+		database.execSQL("CREATE TABLE tmp_location_data (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `time` INTEGER NOT NULL, `lat` REAL NOT NULL, `lon` REAL NOT NULL, `alt` REAL, `hor_acc` REAL, `ver_acc` REAL, `activity` INTEGER NOT NULL, `confidence` INTEGER NOT NULL)")
+		database.execSQL("INSERT INTO tmp_location_data SELECT id, time, lat, lon, alt, hor_acc, null as ver_acc, activity, confidence FROM location_data")
+		database.execSQL("DROP TABLE location_data")
+		database.execSQL("ALTER TABLE tmp_location_data RENAME TO location_data")
+
 		database.execSQL("CREATE TABLE IF NOT EXISTS map_max_heat (`zoom` INTEGER NOT NULL, `maxHeat` REAL NOT NULL, PRIMARY KEY(`zoom`))")
 		database.execSQL("CREATE  INDEX index_location_data_time ON location_data (time)")
 		database.execSQL("CREATE  INDEX index_location_data_lat ON location_data (lat)")
 		database.execSQL("CREATE  INDEX index_location_data_lon ON location_data (lon)")
+
+		database.execSQL("UPDATE location_data SET alt = null WHERE alt = 0.0")
+		database.execSQL("UPDATE location_data SET hor_acc = null WHERE hor_acc = 0.0")
 	}
 }
