32736b71786b4c3f3f1d3188b745031b2e61f46d
adsamcik <adsamcik@users.noreply.github.com>
2019-04-01 20:54:10 +0200

    Refactoring

--- app/src/main/java/com/adsamcik/signalcollector/database/dao/CellDataDao.kt
+++ app/src/main/java/com/adsamcik/signalcollector/database/dao/CellDataDao.kt
@@ -10,7 +10,7 @@ import com.adsamcik.signalcollector.tracker.data.CellType
 interface CellDataDao : BaseDao<DatabaseCellData> {
 
 	@Query("UPDATE cell_data SET location_id = CASE WHEN location_id IS NULL OR asu < :asu THEN :locationId ELSE location_id END, last_seen = :lastSeen, type = :type, asu = CASE WHEN asu < :asu THEN :asu ELSE asu END WHERE id = :id")
-	abstract fun update(id: String, locationId: Long?, lastSeen: Long, type: CellType, asu: Int)
+	fun update(id: String, locationId: Long?, lastSeen: Long, type: CellType, asu: Int)
 
 	@Transaction
 	fun upsert(objList: Collection<DatabaseCellData>) {
@@ -21,8 +21,8 @@ interface CellDataDao : BaseDao<DatabaseCellData> {
 	}
 
 	@Query("SELECT * from cell_data")
-	abstract fun getAll(): List<DatabaseCellData>
+	fun getAll(): List<DatabaseCellData>
 
 	@Query("SELECT COUNT(*) from cell_data")
-	abstract fun count(): Long
+	fun count(): Long
 }
