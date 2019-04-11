60b7c276eaea41f7d1d320ba0ce3f8c4aa3f8f24
adsamcik <adsamcik@users.noreply.github.com>
2019-04-11 14:12:23 +0200

    Tweaked step challenge values

--- app/src/main/java/com/adsamcik/signalcollector/game/challenge/data/definition/StepChallengeDefinition.kt
+++ app/src/main/java/com/adsamcik/signalcollector/game/challenge/data/definition/StepChallengeDefinition.kt
@@ -8,13 +8,13 @@ import com.adsamcik.signalcollector.game.challenge.data.ChallengeType
 import com.adsamcik.signalcollector.game.challenge.data.builder.StepChallengeBuilder
 import com.adsamcik.signalcollector.game.challenge.data.instance.StepChallengeInstance
 
-class StepChallengeDefinition : ChallengeDefinition<StepChallengeInstance>(R.string.challenge_step_title, R.string.challenge_step_description, Constants.DAY_IN_MILLISECONDS) {
-	override val type: ChallengeType
-		get() = ChallengeType.Step
+class StepChallengeDefinition : ChallengeDefinition<StepChallengeInstance>(R.string.challenge_step_title, R.string.challenge_step_description, 2 * Constants.DAY_IN_MILLISECONDS) {
+	override val type: ChallengeType = ChallengeType.Step
 
-	val defaultRequiredStepCount = 10000
+	val defaultRequiredStepCount = 20000
 
-	override val minDurationMultiplier: Double get() = 0.8
+	override val minDurationMultiplier: Double = 0.5
+	override val maxDurationMultiplier: Double = 4.0
 
 	override fun createInstance(context: Context, startAt: Long): StepChallengeInstance {
 		return StepChallengeBuilder(this).build(context, startAt)
