c41b4ad2057ad971cf8be40897e0e03ab5a1272b
adsamcik <adsamcik@users.noreply.github.com>
2019-05-05 16:21:50 +0200

    Refactoring

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -11,29 +11,15 @@ object Android {
 object Libraries {
 	object Versions {
 		const val constraintLayout = "2.0.0-alpha5"
-		const val ktx = "1.1.0-alpha05"
+		const val coreKtx = "1.1.0-alpha05"
 		const val appcompat = "1.1.0-alpha04"
 		const val room = "2.1.0-alpha07"
 		const val kotlin = "1.3.31"
 		const val dokka = "0.9.18"
 		const val moshi = "1.8.0"
 		const val work = "2.0.1"
+		const val lifecycle = "2.1.0-alpha04"
 	}
-
-	private const val moshiBaseString = "com.squareup.moshi:moshi"
-	private const val moshi = "$moshiBaseString:${Versions.moshi}"
-
-	private const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
-
-	private const val appCompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
-	private const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
-	private const val coreKtx = "androidx.core:core-ktx:${Versions.ktx}"
-
-	private const val roomBaseString = "androidx.room:room"
-
-	private const val roomRuntime = "$roomBaseString-runtime:${Versions.room}"
-	private const val roomCompiler = "$roomBaseString-compiler:${Versions.room}"
-	private const val roomKtx = "$roomBaseString-ktx:${Versions.room}"
 
 	private fun DependencyHandler.implementation(name: String) = add("implementation", name)
 	private fun DependencyHandler.kapt(name: String) = add("kapt", name)
@@ -41,32 +27,33 @@ object Libraries {
 
 	fun moshi(dependencyHandler: DependencyHandler) {
 		with(dependencyHandler) {
-			implementation(moshi)
-			kapt(Kapt.moshi)
+			implementation("com.squareup.moshi:moshi:${Versions.moshi}")
+			kapt("com.squareup.moshi:moshi-kotlin-codegen:${Versions.moshi}")
 		}
 	}
 
 	fun database(dependencyHandler: DependencyHandler) {
 		with(dependencyHandler) {
-			implementation(roomRuntime)
-			kapt(roomCompiler)
-			implementation(roomKtx)
-			androidTestImplementation(Test.room)
+			val roomBaseString = "androidx.room:room"
+			implementation("$roomBaseString-runtime:${Versions.room}")
+			kapt("$roomBaseString-compiler:${Versions.room}")
+			implementation("$roomBaseString-ktx:${Versions.room}")
+			androidTestImplementation("androidx.room:room-testing:${Versions.room}")
 		}
 	}
 
 	fun core(dependencyHandler: DependencyHandler) {
 		with(dependencyHandler) {
-			implementation(appCompat)
-			implementation(kotlinStdLib)
-			implementation(coreKtx)
-			implementation(constraintLayout)
-			implementation("androidx.lifecycle:lifecycle-extensions:2.1.0-alpha04")
+			implementation("androidx.appcompat:appcompat:${Versions.appcompat}")
+			implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}")
+			implementation("androidx.core:core-ktx:${Versions.coreKtx}")
+			implementation("androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}")
+			implementation("androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle}")
+			implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.2.1")
 			implementation("androidx.fragment:fragment:1.1.0-alpha07")
 			implementation("com.google.android.material:material:1.1.0-alpha05")
-			implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.2.1")
 
-			kapt("androidx.lifecycle:lifecycle-compiler:2.1.0-alpha04")
+			kapt("androidx.lifecycle:lifecycle-compiler:${Versions.lifecycle}")
 
 			androidTestImplementation("androidx.test:runner:1.1.1")
 			androidTestImplementation("androidx.test.espresso:espresso-core:3.1.1")
@@ -86,14 +73,5 @@ object Libraries {
 			implementation("androidx.work:work-runtime-ktx:${Versions.work}")
 			androidTestImplementation("androidx.work:work-testing:${Versions.work}")
 		}
-	}
-
-
-	private object Kapt {
-		const val moshi = "$moshiBaseString-kotlin-codegen:${Versions.moshi}"
-	}
-
-	private object Test {
-		const val room = "androidx.room:room-testing:${Versions.room}"
 	}
 }
