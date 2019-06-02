9842186a2b56b588300333cfc573de50f5ac7d24
adsamcik <adsamcik@users.noreply.github.com>
2019-06-02 15:42:59 +0200

    Fixed incorrect altitude formatting

--- app/src/main/java/com/adsamcik/signalcollector/tracker/ui/recycler/data/LocationTrackerInfo.kt
+++ app/src/main/java/com/adsamcik/signalcollector/tracker/ui/recycler/data/LocationTrackerInfo.kt
@@ -3,10 +3,15 @@ package com.adsamcik.signalcollector.tracker.ui.recycler.data
 import com.adsamcik.signalcollector.R
 import com.adsamcik.signalcollector.common.Assist
 import com.adsamcik.signalcollector.common.data.Location
+import com.adsamcik.signalcollector.common.misc.extension.formatDistance
+import com.adsamcik.signalcollector.common.preference.Preferences
 import com.adsamcik.signalcollector.tracker.ui.recycler.TrackerInfoAdapter
 
 class LocationTrackerInfo(var location: Location) : TrackerInfo(NAME_RESOURCE, R.drawable.ic_outline_location_on_24px) {
 	override fun bindContent(holder: TrackerInfoAdapter.ViewHolder) {
+		val context = holder.itemView.context
+		val resources = context.resources
+
 		setBoldText(holder) { title, value ->
 			value.text = Assist.coordinateToString(location.latitude)
 			title.setText(R.string.latitude)
@@ -20,7 +25,7 @@ class LocationTrackerInfo(var location: Location) : TrackerInfo(NAME_RESOURCE, R
 		val altitude = location.altitude
 		if (altitude != null) {
 			setBoldText(holder) { title, value ->
-				value.text = Assist.coordinateToString(altitude)
+				value.text = resources.formatDistance(altitude, 1, Preferences.getLengthSystem(context))
 				title.setText(R.string.altitude)
 			}
 		}
