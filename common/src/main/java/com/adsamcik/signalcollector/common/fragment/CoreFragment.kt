bcf2f51a43ea95691b65298ce819c3a2c34cc15e
adsamcik <adsamcik@users.noreply.github.com>
2019-06-15 13:08:26 +0200

    Add Core Fragments

---
+++ common/src/main/java/com/adsamcik/signalcollector/common/fragment/CoreFragment.kt
@@ -0,0 +1,29 @@
+package com.adsamcik.signalcollector.common.fragment
+
+import android.os.Bundle
+import androidx.annotation.CallSuper
+import androidx.fragment.app.Fragment
+import com.adsamcik.signalcollector.common.Reporter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancelChildren
+import kotlin.coroutines.CoroutineContext
+
+abstract class CoreFragment : Fragment(), CoroutineScope {
+	private val job = SupervisorJob()
+
+	override val coroutineContext: CoroutineContext
+		get() = Dispatchers.Main + job
+
+	@CallSuper
+	override fun onCreate(savedInstanceState: Bundle?) {
+		Reporter.initialize(requireContext())
+		super.onCreate(savedInstanceState)
+	}
+
+	override fun onDestroy() {
+		super.onDestroy()
+		coroutineContext.cancelChildren()
+	}
+}
---
+++ common/src/main/java/com/adsamcik/signalcollector/common/fragment/CoreUIFragment.kt
@@ -0,0 +1,65 @@
+package com.adsamcik.signalcollector.common.fragment
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.CallSuper
+import androidx.core.content.ContextCompat
+import com.adsamcik.signalcollector.common.color.ColorController
+import com.adsamcik.signalcollector.common.color.ColorManager
+import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlin.coroutines.CoroutineContext
+
+abstract class CoreUIFragment : CoreFragment() {
+	private var themeLocationRequestCode = 4513
+	private val job = SupervisorJob()
+
+	protected val colorController: ColorController = ColorManager.createController()
+
+	override val coroutineContext: CoroutineContext
+		get() = Dispatchers.Main + job
+
+	@CallSuper
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		initializeColors()
+	}
+
+	@CallSuper
+	override fun onDestroy() {
+		ColorManager.recycleController(colorController)
+		super.onDestroy()
+	}
+
+	private fun initializeColors() {
+		ColorManager.initializeFromPreferences(requireContext())
+		initializeSunriseSunset()
+	}
+
+	@CallSuper
+	override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+		if (requestCode == themeLocationRequestCode) {
+			if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+				initializeSunriseSunset()
+			}
+		}
+	}
+
+	private fun initializeSunriseSunset() {
+		val context = requireContext()
+		val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+
+		if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+			fusedLocationClient.lastLocation.addOnCompleteListener {
+				if (it.isSuccessful) {
+					it.result?.let { location -> ColorManager.setLocation(location) }
+				}
+			}
+		} else if (Build.VERSION.SDK_INT >= 23) {
+			requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), themeLocationRequestCode)
+		}
+	}
+}
