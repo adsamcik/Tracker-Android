91b286158ce9698d205118483575e44f94e62df4
adsamcik <adsamcik@users.noreply.github.com>
2019-06-17 16:45:10 +0200

    Add ActivityRecognizer interface and SessionActivity

---
+++ app/src/main/java/com/adsamcik/signalcollector/activity/SessionActivity.kt
@@ -0,0 +1,17 @@
+package com.adsamcik.signalcollector.activity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//todo add icon
+@Entity(tableName = "session_activity")
+data class SessionActivity(
+		/**
+		 * Name of the activity (Also used as primary key and identifier).
+		 */
+		@PrimaryKey var id: String
+)
+
+data class NativeSessionActivity(val id: String, var titleRes: Int, var iconRes: Int) {
+	val sessionActivity get() = SessionActivity(id)
+}
---
+++ app/src/main/java/com/adsamcik/signalcollector/activity/recognizer/ActivityRecognizer.kt
@@ -0,0 +1,20 @@
+package com.adsamcik.signalcollector.activity.recognizer
+
+import androidx.annotation.IntRange
+import com.adsamcik.signalcollector.activity.SessionActivity
+import com.adsamcik.signalcollector.tracker.data.session.TrackerSession
+
+interface ActivityRecognizer {
+	/**
+	 * Represents how accurately can the algorithm detect activities.
+	 * More accurate algorithms will be preferred.
+	 *
+	 * Algorithms that mostly recognize activity right should have positive value.
+	 * While algorithms that sometimes recognize activity should have negative value.
+	 */
+	val precisionConfidence: Int
+
+	fun resolve(session: TrackerSession): SessionActivity
+}
+
+data class ActivityRecognitionResult(val activityId: Int, @IntRange(from = 0, to = 100) val confidence: Int)
