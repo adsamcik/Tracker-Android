01984d5557da9c1a8813794cc086c68e38aa6870
adsamcik <adsamcik@users.noreply.github.com>
2019-07-23 21:53:16 +0200

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
 		applicationId = "com.adsamcik.signalcollector"
 		minSdkVersion(Android.min)
 		targetSdkVersion(Android.target)
-		versionCode = 306
-		versionName = "7.0α23 Offline"
+		versionCode = 307
+		versionName = "7.0α24 Offline"
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
 
