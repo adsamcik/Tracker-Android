557f427c8f40a4e5aa5c6af5075a49e02e0fc65c
adsamcik <adsamcik@users.noreply.github.com>
2019-08-07 10:29:32 +0200

    Refactoring

--- activity/build.gradle.kts
+++ activity/build.gradle.kts
@@ -1,62 +1,62 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("com.android.library")
-    Libraries.corePlugins(this)
+	id("com.android.library")
+	Libraries.corePlugins(this)
 }
 
 android {
-    compileSdkVersion(Android.compile)
+	compileSdkVersion(Android.compile)
 
-    defaultConfig {
-        minSdkVersion(Android.min)
-        targetSdkVersion(Android.target)
-        versionCode = 1
-        versionName = "1.0"
+	defaultConfig {
+		minSdkVersion(Android.min)
+		targetSdkVersion(Android.target)
+		versionCode = 1
+		versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        kapt {
-            arguments {
-                this.arg("room.schemaLocation", "$projectDir/schemas")
-            }
-        }
-    }
+		kapt {
+			arguments {
+				this.arg("room.schemaLocation", "$projectDir/schemas")
+			}
+		}
+	}
 
-    sourceSets {
-        this.maybeCreate("androidTest").assets.srcDirs(files("$projectDir/schemas"))
-    }
+	sourceSets {
+		this.maybeCreate("androidTest").assets.srcDirs(files("$projectDir/schemas"))
+	}
 
-    with(compileOptions) {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
+	with(compileOptions) {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
+	}
 
-    tasks.withType<KotlinCompile> {
-        with(kotlinOptions) {
-            jvmTarget = "1.8"
-        }
-    }
+	tasks.withType<KotlinCompile> {
+		with(kotlinOptions) {
+			jvmTarget = "1.8"
+		}
+	}
 
-    buildTypes {
-        getByName("debug") {
-            isTestCoverageEnabled = true
-        }
+	buildTypes {
+		getByName("debug") {
+			isTestCoverageEnabled = true
+		}
 
-        create("release_nominify") {
-            isMinifyEnabled = false
-        }
-        getByName("release") {
-            isMinifyEnabled = true
-        }
-    }
+		create("release_nominify") {
+			isMinifyEnabled = false
+		}
+		getByName("release") {
+			isMinifyEnabled = true
+		}
+	}
 }
 
 dependencies {
-    implementation(project(":common"))
+	implementation(project(":common"))
 
-    Libraries.core(this)
-    Libraries.crashlytics(this)
-    Libraries.location(this)
-    Libraries.test(this)
+	Libraries.core(this)
+	Libraries.crashlytics(this)
+	Libraries.location(this)
+	Libraries.test(this)
 }
