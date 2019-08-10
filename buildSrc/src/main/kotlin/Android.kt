60202fddcaf2e6c51daf274b8638f2f8baad1678
adsamcik <adsamcik@users.noreply.github.com>
2019-08-10 14:34:17 +0200

    Updated build tools

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -1,7 +1,7 @@
 object Android {
-	const val min = 21
-	const val compile = "android-29"
-	const val target = "29"
+	const val min: Int = 21
+	const val compile: String = "android-29"
+	const val target: String = "29"
 
-	const val buildTools = "29.0.1"
+	const val buildTools: String = "29.0.2"
 }
