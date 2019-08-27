22ecadac155b168561027941380fb457ad02c550
adsamcik <adsamcik@users.noreply.github.com>
2019-08-27 13:32:31 +0200

    Refactoring and Suppressed issues (This is considered library function atm an thus is allowed to be of lower quality)

--- common/src/main/java/com/adsamcik/tracker/common/style/utility/PalleteGenerator.kt
+++ common/src/main/java/com/adsamcik/tracker/common/style/utility/PalleteGenerator.kt
@@ -15,6 +15,7 @@ import kotlin.math.pow
 import kotlin.math.round
 import kotlin.math.sqrt
 import kotlin.random.Random
+
 /**
 chroma.palette-gen.js - a palette generator for data scientists
 based on Chroma.js HCL color space
@@ -35,16 +36,15 @@ that code without the copy of the GNU GPL normally required by
 section 4, provided you include this license notice and a URL
 through which recipients can access the Corresponding Source.
  */
+@Suppress("ComplexMethod", "LongMethod", "MagicNumber", "NestedBlockDepth", "LongParameterList")
 class PalleteGenerator {
-
 	data class LabColor(var l: Double, var a: Double, var b: Double) {
-
 		fun toRgb(): Int = ColorUtils.LABToColor(l, a, b)
 	}
 
 	enum class DistanceType {
 		Default,
-		Euclidian,
+		Euclidean,
 		CMC,
 		Compromise,
 		CB_Protanope,
@@ -54,10 +54,10 @@ class PalleteGenerator {
 
 	fun generate(
 			colorsCount: Int = 8,
-			checkColor: (Int) -> Boolean = { true },
+			checkColor: (DoubleArray) -> Boolean = { true },
 			forceMode: Boolean = false,
 			quality: Int = 50,
-			ultra_precision: Boolean = false,
+			ultraPrecision: Boolean = false,
 			distanceType: DistanceType = DistanceType.Default
 	): List<LabColor> {
 		Log.d(
@@ -71,11 +71,9 @@ class PalleteGenerator {
 			val colors = mutableListOf<DoubleArray>()
 
 			// It will be necessary to check if a Lab color exists in the rgb space.
-			/*fun checkLab(lab) {
-				ColorUtils.LABToColor()
-				var color = chroma.lab(lab[0], lab[1], lab[2]);
-				return ns.validateLab(lab) && checkColor(color);
-			}*/
+			fun checkLab(lab: DoubleArray): Boolean {
+				return validateLab(lab) && checkColor(lab);
+			}
 
 			// Init
 			for (i in 0 until colorsCount) {
@@ -107,44 +105,44 @@ class PalleteGenerator {
 
 						// repulsion force
 						val dl = colorA[0] - colorB[0]
-						val da = colorA[1] - colorB[1];
-						val db = colorA[2] - colorB[2];
+						val da = colorA[1] - colorB[1]
+						val db = colorA[2] - colorB[2]
 						val d = getColorDistance(colorA, colorB, distanceType)
 						if (d > 0) {
-							val force = repulsion / d.pow(2);
+							val force = repulsion / d.pow(2)
 
-							vectors[i].l += dl * force / d;
-							vectors[i].a += da * force / d;
-							vectors[i].b += db * force / d;
+							vectors[i].l += dl * force / d
+							vectors[i].a += da * force / d
+							vectors[i].b += db * force / d
 
-							vectors[j].l -= dl * force / d;
-							vectors[j].a -= da * force / d;
-							vectors[j].b -= db * force / d;
+							vectors[j].l -= dl * force / d
+							vectors[j].a -= da * force / d
+							vectors[j].b -= db * force / d
 						} else {
 							// Jitter
-							vectors[j].l += 2 - 4 * Random.nextDouble();
-							vectors[j].a += 2 - 4 * Random.nextDouble();
-							vectors[j].b += 2 - 4 * Random.nextDouble();
+							vectors[j].l += 2 - 4 * Random.nextDouble()
+							vectors[j].a += 2 - 4 * Random.nextDouble()
+							vectors[j].b += 2 - 4 * Random.nextDouble()
 						}
 					}
 				}
 				// Apply Force
 				for (i in 0 until colors.size) {
-					val color = colors[i];
+					val color = colors[i]
 					val displacement = speed * sqrt(
 							vectors[i].l.pow(2) + vectors[i].a.pow(2) + vectors[i].b.pow(
 									2
 							)
-					);
+					)
 					if (displacement > 0) {
-						val ratio = speed * min(0.1, displacement) / displacement;
+						val ratio = speed * min(0.1, displacement) / displacement
 						val candidateLab = doubleArrayOf(
 								color[0] + vectors[i].l * ratio,
 								color[1] + vectors[i].a * ratio,
 								color[2] + vectors[i].b * ratio
 						)
 						//if (checkLab(candidateLab)) {
-						colors[i] = candidateLab;
+						colors[i] = candidateLab
 						//}
 					}
 				}
@@ -154,29 +152,27 @@ class PalleteGenerator {
 		} else {
 
 			// K-Means Mode
-			/*fun checkColor2(lab) {
+			fun checkColor2(lab: DoubleArray): Boolean {
 				// Check that a color is valid: it must verify our checkColor condition, but also be in the color space
-				var color = chroma.lab(lab);
-				var hcl = color.hcl();
-				return ns.validateLab(lab) && checkColor(color);
-			}*/
+				return validateLab(lab) && checkColor(lab)
+			}
 
-			var kMeans = mutableListOf<DoubleArray>()
+			val kMeans = mutableListOf<DoubleArray>()
 			for (i in 0 until colorsCount) {
 				var lab = doubleArrayOf(
 						100 * Random.nextDouble(),
 						100 * (2 * Random.nextDouble() - 1),
 						100 * (2 * Random.nextDouble() - 1)
 				)
-				val failsafe = 10;
-				//while (!checkColor2(lab) && failsafe-- > 0) {
-				lab = doubleArrayOf(
-						100 * Random.nextDouble(),
-						100 * (2 * Random.nextDouble() - 1),
-						100 * (2 * Random.nextDouble() - 1)
-				)
-				//}
-				kMeans.add(lab);
+				var iterationIndex = 0
+				while (!checkColor2(lab) && iterationIndex++ < 10) {
+					lab = doubleArrayOf(
+							100 * Random.nextDouble(),
+							100 * (2 * Random.nextDouble() - 1),
+							100 * (2 * Random.nextDouble() - 1)
+					)
+				}
+				kMeans.add(lab)
 			}
 
 
@@ -187,7 +183,7 @@ class PalleteGenerator {
 			val stepA: Int
 			val stepB: Int
 
-			if (ultra_precision) {
+			if (ultraPrecision) {
 				stepL = 1
 				stepA = 5
 				stepB = 5
@@ -201,42 +197,42 @@ class PalleteGenerator {
 				for (a in -100..100 step stepA) {
 					for (b in -100..100 step stepB) {
 						//if (checkColor2([l, a, b])) {
-						colorSamples.add(doubleArrayOf(l.toDouble(), a.toDouble(), b.toDouble()));
-						samplesClosest.add(null);
+						colorSamples.add(doubleArrayOf(l.toDouble(), a.toDouble(), b.toDouble()))
+						samplesClosest.add(null)
 						//}
 					}
 				}
 			}
 
 			// Steps
-			var steps = quality;
+			var steps = quality
 			while (steps-- > 0) {
 				// kMeans -> Samples Closest
 				for (i in 0 until colorSamples.size) {
-					val lab = colorSamples[i];
-					var minDistance = Double.POSITIVE_INFINITY;
+					val lab = colorSamples[i]
+					var minDistance = Double.POSITIVE_INFINITY
 					for (j in kMeans.indices) {
-						val kMean = kMeans[j];
+						val kMean = kMeans[j]
 						//ns.getColorDistance(lab, kMean, distanceType) replaced with ColorUtils euclidean
 						val distance = ColorUtils.distanceEuclidean(lab, kMean)
 						if (distance < minDistance) {
-							minDistance = distance;
-							samplesClosest[i] = j;
+							minDistance = distance
+							samplesClosest[i] = j
 						}
 					}
 				}
 
 				// Samples -> kMeans
-				var freeColorSamples = colorSamples.toList();
+				var freeColorSamples = colorSamples.toList()
 				for (j in 0 until kMeans.size) {
-					var count = 0;
+					var count = 0
 					val candidateKMean = doubleArrayOf(0.0, 0.0, 0.0)
 					for (i in 0 until colorSamples.size) {
 						if (samplesClosest[i] == j) {
-							count++;
-							candidateKMean[0] += colorSamples[i][0];
-							candidateKMean[1] += colorSamples[i][1];
-							candidateKMean[2] += colorSamples[i][2];
+							count++
+							candidateKMean[0] += colorSamples[i][0]
+							candidateKMean[1] += colorSamples[i][1]
+							candidateKMean[2] += colorSamples[i][2]
 						}
 					}
 
@@ -248,13 +244,13 @@ class PalleteGenerator {
 					}
 //&& checkColor2([candidateKMean[0], candidateKMean[1], candidateKMean[2]]) && candidateKMean
 					if (count != 0) {
-						kMeans[j] = candidateKMean;
+						kMeans[j] = candidateKMean
 					} else {
 						// The candidate kMean is out of the boundaries of the color space, or unfound.
 						if (freeColorSamples.isNotEmpty()) {
 							// We just search for the closest FREE color of the candidate kMean
-							var minDistance = Double.POSITIVE_INFINITY;
-							var closest = -1;
+							var minDistance = Double.POSITIVE_INFINITY
+							var closest = -1
 							for (i in freeColorSamples.indices) {
 
 								//ns.getColorDistance(freeColorSamples[i],candidateKMean,distanceType );
@@ -263,17 +259,18 @@ class PalleteGenerator {
 										candidateKMean
 								)
 								if (distance < minDistance) {
-									minDistance = distance;
-									closest = i;
+									minDistance = distance
+									closest = i
 								}
 							}
-							if (closest >= 0)
-								kMeans[j] = colorSamples[closest];
+							if (closest >= 0) {
+								kMeans[j] = colorSamples[closest]
+							}
 
 						} else {
 							// Then we just search for the closest color of the candidate kMean
-							var minDistance = Double.POSITIVE_INFINITY;
-							var closest = -1;
+							var minDistance = Double.POSITIVE_INFINITY
+							var closest = -1
 							for (i in 0 until colorSamples.size) {
 								//ns.getColorDistance(colorSamples[i],candidateKMean,distanceType)
 								val distance = ColorUtils.distanceEuclidean(
@@ -281,16 +278,17 @@ class PalleteGenerator {
 										candidateKMean
 								)
 								if (distance < minDistance) {
-									minDistance = distance;
-									closest = i;
+									minDistance = distance
+									closest = i
 								}
 							}
-							if (closest >= 0)
-								kMeans[j] = colorSamples[closest];
+							if (closest >= 0) {
+								kMeans[j] = colorSamples[closest]
+							}
 						}
 					}
 					freeColorSamples = freeColorSamples.filter { color ->
-						color[0] != kMeans[j][0] || color[1] != kMeans[j][1] || color[2] != kMeans[j][2];
+						color[0] != kMeans[j][0] || color[1] != kMeans[j][1] || color[2] != kMeans[j][2]
 					}
 				}
 			}
@@ -303,10 +301,10 @@ class PalleteGenerator {
 		val colorsToSort = inputColors.toMutableList()
 		val diffColors = mutableListOf(colorsToSort.removeAt(0))
 		while (colorsToSort.isNotEmpty()) {
-			var index = -1;
-			var maxDistance = -1.0;
+			var index = -1
+			var maxDistance = -1.0
 			for (candidate_index in 0 until colorsToSort.size) {
-				var d = Double.POSITIVE_INFINITY;
+				var d = Double.POSITIVE_INFINITY
 				for (i in diffColors.size - 1 downTo 0) {
 					val colorA = DoubleArray(3)
 					ColorUtils.colorToLAB(colorsToSort[candidate_index], colorA)
@@ -320,17 +318,17 @@ class PalleteGenerator {
 					if (d < Double.POSITIVE_INFINITY) break
 				}
 				if (d > maxDistance) {
-					maxDistance = d;
-					index = candidate_index;
+					maxDistance = d
+					index = candidate_index
 				}
 			}
-			val color = colorsToSort[index];
-			diffColors.add(color);
+			val color = colorsToSort[index]
+			diffColors.add(color)
 
 			colorsToSort.removeAt(index)
 			//colorsToSort = colorsToSort.filter(fun(c, i) { return i != index; });
 		}
-		return diffColors.toIntArray();
+		return diffColors.toIntArray()
 	}
 
 	fun getColorDistance(
@@ -379,9 +377,9 @@ class PalleteGenerator {
 		}
 
 		fun distanceColorblind(lab1: DoubleArray, lab2: DoubleArray, type: DistanceType): Double {
-			val lab1Cb = simulate(lab1, type);
-			val lab2Cb = simulate(lab2, type);
-			return cmcDistance(lab1Cb, lab2Cb, 2.0, 1.0);
+			val lab1Cb = simulate(lab1, type)
+			val lab2Cb = simulate(lab2, type)
+			return cmcDistance(lab1Cb, lab2Cb, 2.0, 1.0)
 		}
 
 		fun compromiseDistance(lab1: DoubleArray, lab2: DoubleArray): Double {
@@ -394,28 +392,28 @@ class PalleteGenerator {
 					DistanceType.CB_Deuteranope,
 					DistanceType.CB_Tritanope
 			)
-			types.forEach(fun(type) {
-				val lab1Cb = simulate(lab1, type);
-				val lab2Cb = simulate(lab2, type);
+			types.forEach { type ->
+				val lab1Cb = simulate(lab1, type)
+				val lab2Cb = simulate(lab2, type)
 				if (!(lab1Cb.any { isNaN(it) } || lab2Cb.any { isNaN(it) })) {
 
 					val c = when (type) {
-						DistanceType.CB_Protanope -> 100;
-						DistanceType.CB_Deuteranope -> 500;
-						DistanceType.CB_Tritanope -> 1;
+						DistanceType.CB_Protanope -> 100
+						DistanceType.CB_Deuteranope -> 500
+						DistanceType.CB_Tritanope -> 1
 						else -> throw IllegalArgumentException(type.toString())
 					}
 					distances.add(cmcDistance(lab1Cb, lab2Cb, 2.0, 1.0))
 					coefficients.add(c)
 				}
-			})
+			}
 			var total = 0.0
 			var count = 0
 			distances.forEachIndexed { index, d ->
 				total += coefficients[index] * d
 				count += coefficients[index]
 			}
-			return total / count;
+			return total / count
 		}
 
 		fun euclideanDistance(lab1: DoubleArray, lab2: DoubleArray): Double {
@@ -428,7 +426,7 @@ class PalleteGenerator {
 
 		return when (type) {
 			DistanceType.Default -> euclideanDistance(lab1, lab2)
-			DistanceType.Euclidian -> euclideanDistance(lab1, lab2)
+			DistanceType.Euclidean -> euclideanDistance(lab1, lab2)
 			DistanceType.CMC -> cmcDistance(lab1, lab2, 2.0, 1.0)
 			DistanceType.Compromise -> compromiseDistance(lab1, lab2)
 			else -> distanceColorblind(lab1, lab2, type)
@@ -437,7 +435,7 @@ class PalleteGenerator {
 
 	data class ConfusionData(val x: Double, val y: Double, val m: Double, val yint: Double)
 
-	val confusionLines = mapOf(
+	private val confusionLines = mapOf(
 			DistanceType.CB_Protanope to ConfusionData(
 					x = 0.7465,
 					y = 0.2535,
@@ -458,7 +456,7 @@ class PalleteGenerator {
 			)
 	)
 
-	val simulateCache = mutableMapOf<String, DoubleArray>()
+	private val simulateCache = mutableMapOf<String, DoubleArray>()
 
 	fun simulate(lab: DoubleArray, type: DistanceType, amount: Int = 1): DoubleArray {
 		// WARNING: may return [NaN, NaN, NaN]
@@ -469,10 +467,10 @@ class PalleteGenerator {
 		if (cache != null) return cache
 
 		// Get data from type
-		val confuseX = confusionLines.getValue(type).x;
-		val confuseY = confusionLines.getValue(type).y;
-		val confuseM = confusionLines.getValue(type).m;
-		val confuseYint = confusionLines.getValue(type).yint;
+		val confuseX = confusionLines.getValue(type).x
+		val confuseY = confusionLines.getValue(type).y
+		val confuseM = confusionLines.getValue(type).m
+		val confuseYint = confusionLines.getValue(type).yint
 
 		// Code adapted from http://galacticmilk.com/labs/Color-Vision/Javascript/Color.Vision.Simulate.js
 		val color = LabColor(lab[0], lab[1], lab[2]).toRgb()
@@ -480,37 +478,37 @@ class PalleteGenerator {
 		val sg = color.green.toDouble()
 		val sb = color.blue.toDouble()
 		// Convert source color into XYZ color space
-		val powR = sr.pow(2.2);
-		val powG = sg.pow(2.2);
-		val powB = sb.pow(2.2);
-		val x = powR * 0.412424 + powG * 0.357579 + powB * 0.180464; // RGB->XYZ (sRGB:D65)
-		val y = powR * 0.212656 + powG * 0.715158 + powB * 0.0721856;
-		val z = powR * 0.0193324 + powG * 0.119193 + powB * 0.950444;
+		val powR = sr.pow(2.2)
+		val powG = sg.pow(2.2)
+		val powB = sb.pow(2.2)
+		val x = powR * 0.412424 + powG * 0.357579 + powB * 0.180464 // RGB->XYZ (sRGB:D65)
+		val y = powR * 0.212656 + powG * 0.715158 + powB * 0.0721856
+		val z = powR * 0.0193324 + powG * 0.119193 + powB * 0.950444
 		// Convert XYZ into xyY Chromacity Coordinates (xy) and Luminance (Y)
-		val chroma_x = x / (x + y + z);
-		val chroma_y = y / (x + y + z);
+		val chromaX = x / (x + y + z)
+		val chromaY = y / (x + y + z)
 		// Generate the "Confusion Line" between the source color and the Confusion Point
-		val m = (chroma_y - confuseY) / (chroma_x - confuseX); // slope of Confusion Line
-		val yint = chroma_y - chroma_x * m; // y-intercept of confusion line (x-intercept = 0.0)
+		val m = (chromaY - confuseY) / (chromaX - confuseX) // slope of Confusion Line
+		val yint = chromaY - chromaX * m // y-intercept of confusion line (x-intercept = 0.0)
 		// How far the xy coords deviate from the simulation
-		val deviate_x = (confuseYint - yint) / (m - confuseM);
-		val deviate_y = (m * deviate_x) + yint;
+		val deviateX = (confuseYint - yint) / (m - confuseM)
+		val deviateY = (m * deviateX) + yint
 		// Compute the simulated color"s XYZ coords
-		val dX = deviate_x * y / deviate_y;
-		val dZ = (1.0 - (deviate_x + deviate_y)) * y / deviate_y;
+		val dX = deviateX * y / deviateY
+		val dZ = (1.0 - (deviateX + deviateY)) * y / deviateY
 		// Neutral grey calculated from luminance (in D65)
-		val neutral_X = 0.312713 * y / 0.329016;
-		val neutral_Z = 0.358271 * y / 0.329016;
+		val neutralX = 0.312713 * y / 0.329016
+		val neutralZ = 0.358271 * y / 0.329016
 		// Difference between simulated color and neutral grey
-		val diffX = neutral_X - dX;
-		val diffZ = neutral_Z - dZ;
-		val diffR = diffX * 3.24071 + diffZ * -0.498571; // XYZ->RGB (sRGB:D65)
-		val diffG = diffX * -0.969258 + diffZ * 0.0415557;
-		val diffB = diffX * 0.0556352 + diffZ * 1.05707;
+		val diffX = neutralX - dX
+		val diffZ = neutralZ - dZ
+		val diffR = diffX * 3.24071 + diffZ * -0.498571 // XYZ->RGB (sRGB:D65)
+		val diffG = diffX * -0.969258 + diffZ * 0.0415557
+		val diffB = diffX * 0.0556352 + diffZ * 1.05707
 		// Convert to RGB color space
-		var dr = dX * 3.24071 + y * -1.53726 + dZ * -0.498571; // XYZ->RGB (sRGB:D65)
-		var dg = dX * -0.969258 + y * 1.87599 + dZ * 0.0415557;
-		var db = dX * 0.0556352 + y * -0.203996 + dZ * 1.05707;
+		var dr = dX * 3.24071 + y * -1.53726 + dZ * -0.498571 // XYZ->RGB (sRGB:D65)
+		var dg = dX * -0.969258 + y * 1.87599 + dZ * 0.0415557
+		var db = dX * 0.0556352 + y * -0.203996 + dZ * 1.05707
 
 		fun isPositive(value: Double) = if (value >= 0) 1.0 else 0.0
 
@@ -518,34 +516,34 @@ class PalleteGenerator {
 				if (value in range) value else 0.0
 
 		// Compensate simulated color towards a neutral fit in RGB space
-		val fitR = (isPositive(dr) - dr) / diffR;
-		val fitG = (isPositive(dg) - dg) / diffG;
-		val fitB = (isPositive(db) - db) / diffB;
+		val fitR = (isPositive(dr) - dr) / diffR
+		val fitG = (isPositive(dg) - dg) / diffG
+		val fitB = (isPositive(db) - db) / diffB
 		val range = 0.0..1.0
 		val adjust = maxOf( // highest value
 				keepOnlyIfInside(range, fitR),
 				keepOnlyIfInside(range, fitG),
 				keepOnlyIfInside(range, fitB)
-		);
+		)
 		// Shift proportional to the greatest shift
-		dr += (adjust * diffR);
-		dg += (adjust * diffG);
-		db += (adjust * diffB);
+		dr += (adjust * diffR)
+		dg += (adjust * diffG)
+		db += (adjust * diffB)
 		// Apply gamma correction
-		dr = dr.pow(1.0 / 2.2);
-		dg = dg.pow(1.0 / 2.2);
-		db = db.pow(1.0 / 2.2);
+		dr = dr.pow(1.0 / 2.2)
+		dg = dg.pow(1.0 / 2.2)
+		db = db.pow(1.0 / 2.2)
 		// Anomylize colors
-		dr = sr * (1.0 - amount) + dr * amount;
-		dg = sg * (1.0 - amount) + dg * amount;
-		db = sb * (1.0 - amount) + db * amount;
+		dr = sr * (1.0 - amount) + dr * amount
+		dg = sg * (1.0 - amount) + dg * amount
+		db = sb * (1.0 - amount) + db * amount
 		val result = DoubleArray(3)
 		ColorUtils.RGBToLAB((dr * 255).toInt(), (dg * 255).toInt(), (db * 255).toInt(), result)
 		simulateCache[key] = result
 		return result
 	}
 
-	fun xyzToRgb(r: Double): Double {
+	private fun xyzToRgb(r: Double): Double {
 		val value = if (r <= 0.00304) {
 			12.92 * r
 		} else {
@@ -554,7 +552,7 @@ class PalleteGenerator {
 		return round(255.0 * value)
 	}
 
-	fun labToXyz(t: Double): Double {
+	private fun labToXyz(t: Double): Double {
 		return if (t > LabConstants.t1) t.pow(3) else (LabConstants.t2 * (t - LabConstants.t0))
 	}
 
