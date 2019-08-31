493545a3be3fc32809baa2f19ab3be17a63201e4
adsamcik <adsamcik@users.noreply.github.com>
2019-08-31 16:05:21 +0200

    Refactoring

--- common/src/main/java/com/adsamcik/tracker/common/dialog/DateTimeRangeDialog.kt
+++ common/src/main/java/com/adsamcik/tracker/common/dialog/DateTimeRangeDialog.kt
@@ -18,9 +18,7 @@ fun MaterialDialog.dateTimeRangePicker(
 		selectedRange: LongRange,
 		successCallback: (range: LongRange) -> Unit
 ): MaterialDialog {
-	if (allowedRange.last <= allowedRange.first) {
-		throw IllegalArgumentException()
-	}
+	require(allowedRange.last > allowedRange.first)
 
 	val minDate = Date(allowedRange.first)
 	val maxDate = Date(allowedRange.last + Time.DAY_IN_MILLISECONDS)
