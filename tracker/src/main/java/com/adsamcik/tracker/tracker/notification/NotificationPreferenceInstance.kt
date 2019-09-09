f18c149b726acf4e43402330bc5c6420f6da1386
adsamcik <adsamcik@users.noreply.github.com>
2019-09-09 19:18:52 +0200

    Removed unused data class

--- tracker/src/main/java/com/adsamcik/tracker/tracker/notification/NotificationPreferenceInstance.kt
+++
@@ -1,8 +0,0 @@
-package com.adsamcik.tracker.tracker.notification
-
-import com.adsamcik.tracker.common.database.data.NotificationPreference
-
-data class NotificationPreferenceInstance(
-		val preference: NotificationPreference,
-		val titleRes: Int
-)
