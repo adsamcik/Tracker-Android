b44c6da3df2aa36fb726b4d8cd581785b6d742e4
adsamcik <adsamcik@users.noreply.github.com>
2019-09-10 17:45:12 +0200

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
 		applicationId = "com.adsamcik.signalcollector"
 		minSdkVersion(Android.min)
 		targetSdkVersion(Android.target)
-		versionCode = 321
-		versionName = "2019.1ι8"
+		versionCode = 322
+		versionName = "2019.1ι9"
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		resConfigs("en", "cs")
 	}
