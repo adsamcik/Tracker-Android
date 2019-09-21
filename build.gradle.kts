30ad89dbeafca53f68e0c4e61a3f7fd4f9768113
adsamcik <adsamcik@users.noreply.github.com>
2019-09-21 20:40:26 +0200

    Updated fabric gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
 		classpath("com.android.tools.build:gradle:3.5.0")
 		classpath("com.google.gms:google-services:4.3.2")
 		classpath("com.google.gms:oss-licenses:0.9.2")
-		classpath("io.fabric.tools:gradle:1.31.0")
+		classpath("io.fabric.tools:gradle:1.31.1")
 
 		classpath("org.jetbrains.dokka:dokka-android-gradle-plugin:${Dependencies.Versions.dokka}")
 		classpath(kotlin("gradle-plugin", Dependencies.Versions.kotlin))
