d8fe3e4c9f73b0b50b72a57193d90b9c91913392
adsamcik <adsamcik@users.noreply.github.com>
2019-09-21 17:02:16 +0200

    Tweaked values of Wifi heatmap

--- map/src/main/java/com/adsamcik/tracker/map/heatmap/creators/WifiHeatmapTileCreator.kt
+++ map/src/main/java/com/adsamcik/tracker/map/heatmap/creators/WifiHeatmapTileCreator.kt
@@ -21,7 +21,7 @@ internal class WifiHeatmapTileCreator(context: Context, val layerData: MapLayerD
 
 		return HeatmapConfig(
 				HeatmapColorScheme.fromArray(heatmapColors, 100),
-				20f,
+				50f,
 				false,
 				{ current, _, stampValue, weight ->
 					current + stampValue * weight
@@ -32,10 +32,10 @@ internal class WifiHeatmapTileCreator(context: Context, val layerData: MapLayerD
 
 	override fun generateStamp(heatmapSize: Int, zoom: Int, pixelInMeters: Float): HeatmapStamp {
 		return HeatmapStamp.generateNonlinear(
-				ceil(APPROXIMATE_DISTANCE_IN_METERS / pixelInMeters).toInt()
+				ceil(APPROXIMATE_DISTANCE_IN_METERS / (pixelInMeters / VISUAL_SCALE)).toInt()
 		) {
-			// very very simplified formula for signal loss
-			(10 * LOSS_EXPONENT * log10(max(it * APPROXIMATE_DISTANCE_IN_METERS, 1f))) / 58.6273f
+			// very very simplified formula for signal loss, but it looks nice
+			(10 * LOSS_EXPONENT * log10(max(it * APPROXIMATE_DISTANCE_IN_METERS, 1f))) / NORMALIZER
 		}
 	}
 
@@ -53,11 +53,19 @@ internal class WifiHeatmapTileCreator(context: Context, val layerData: MapLayerD
 	override val getAllInside get() = dao::getAllInside
 
 	companion object {
+		/**
+		 * This essentially makes many variables in the formula meaningless,
+		 * but they are kept there for readability
+		 */
+		private const val NORMALIZER = 58.6273f
+
 		// , path loss can be represented by the path loss exponent, whose value is normally in the range of 2 to 4
 		// (where 2 is for propagation in free space, 4 is for relatively lossy environments and for the case of
 		// full specular reflection from the earth surface—the so-called flat earth model).
 		private const val LOSS_EXPONENT = 3f
 
 		private const val APPROXIMATE_DISTANCE_IN_METERS = 90f
+
+		private const val VISUAL_SCALE = 2f
 	}
 }
