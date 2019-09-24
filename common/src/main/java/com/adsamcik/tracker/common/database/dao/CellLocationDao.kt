922e7201f84480ed02c9c57e82d78a38d3084fda
adsamcik <adsamcik@users.noreply.github.com>
2019-09-24 08:49:46 +0200

    Fixed SQL query to count unique cells

--- common/src/main/java/com/adsamcik/tracker/common/database/dao/CellLocationDao.kt
+++ common/src/main/java/com/adsamcik/tracker/common/database/dao/CellLocationDao.kt
@@ -24,8 +24,17 @@ interface CellLocationDao : BaseDao<DatabaseCellLocation> {
 	): List<Database2DLocationWeightedMinimal>
 
 	@Query(
-			"""SELECT lat, lon, type as weight FROM cell_location
-			WHERE time >= :from and time <= :to and lat >= :bottomLatitude and lon >= :leftLongitude and lat <= :topLatitude and lon <= :rightLongitude"""
+			"""
+				SELECT lat, lon, type as weight
+				FROM cell_location
+				WHERE
+					time >= :from and
+					time <= :to and
+					lat >= :bottomLatitude and
+					lon >= :leftLongitude and
+					lat <= :topLatitude and
+					lon <= :rightLongitude
+				"""
 	)
 	fun getAllInsideAndBetween(
 			from: Long,
@@ -39,7 +48,7 @@ interface CellLocationDao : BaseDao<DatabaseCellLocation> {
 	@Query("SELECT COUNT(*) from cell_location")
 	fun count(): Long
 
-	@Query("SELECT COUNT(*) from cell_location GROUP BY mcc, mnc, cell_id")
+	@Query("SELECT COUNT(*) FROM (SELECT COUNT(*) from cell_location GROUP BY mcc, mnc, cell_id)")
 	fun uniqueCount(): Long
 
 	@Query("SELECT MIN(time) as start, MAX(time) as endInclusive from cell_location")
