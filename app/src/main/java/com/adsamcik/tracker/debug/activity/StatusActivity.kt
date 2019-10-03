6e0e4065d34b1c88a92b225bab4c9134ce305281
adsamcik <adsamcik@users.noreply.github.com>
2019-10-03 17:24:12 +0200

    Add isLocked to StatusActivity

--- app/src/main/java/com/adsamcik/tracker/debug/activity/StatusActivity.kt
+++ app/src/main/java/com/adsamcik/tracker/debug/activity/StatusActivity.kt
@@ -9,12 +9,17 @@ import android.widget.TextView
 import androidx.work.WorkManager
 import com.adsamcik.tracker.common.activity.DetailActivity
 import com.adsamcik.tracker.common.extension.dp
+import com.adsamcik.tracker.common.extension.requireValue
 import com.adsamcik.tracker.tracker.locker.TrackerLocker
 
 /**
  * Debug Activity used for displaying states of some parts of the app
  */
 class StatusActivity : DetailActivity() {
+
+	override fun onConfigure(configuration: Configuration) {
+		configuration.useColorControllerForContent = false
+	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -40,7 +45,15 @@ class StatusActivity : DetailActivity() {
 				layout,
 				lastId
 		)
-		createPair(
+
+		lastId = createPair(
+				"Is locked",
+				TrackerLocker.isLocked.requireValue.toString(),
+				layout,
+				lastId
+		)
+
+		lastId = createPair(
 				"Has active wait for recharge job",
 				isWaitingForRecharge.toString(),
 				layout,
