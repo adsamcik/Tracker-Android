3e6ce03b754948e53bb2738c9e09d59329d96bb1
adsamcik <adsamcik@users.noreply.github.com>
2019-10-05 12:32:13 +0200

    Improved LocationTrackerTimer concurrency stability

--- tracker/src/main/java/com/adsamcik/tracker/tracker/component/timer/LocationTrackerTimer.kt
+++ tracker/src/main/java/com/adsamcik/tracker/tracker/component/timer/LocationTrackerTimer.kt
@@ -8,6 +8,8 @@ import com.adsamcik.tracker.common.data.LocationData
 import com.adsamcik.tracker.tracker.component.TrackerTimerComponent
 import com.adsamcik.tracker.tracker.component.TrackerTimerReceiver
 import com.adsamcik.tracker.tracker.data.collection.MutableCollectionTempData
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 internal abstract class LocationTrackerTimer : TrackerTimerComponent {
 	protected var receiver: TrackerTimerReceiver? = null
@@ -18,6 +20,8 @@ internal abstract class LocationTrackerTimer : TrackerTimerComponent {
 
 	private var startedAtElapsedRealtimeNanos: Long = Long.MIN_VALUE
 
+	protected val newDataLock = ReentrantLock()
+
 	private fun isLocationYoungEnough(location: Location, previousLocation: Location): Boolean {
 		val locationAge = location.elapsedRealtimeNanos - previousLocation.elapsedRealtimeNanos
 		val maxAge = PREVIOUS_LOCATION_MAX_AGE_IN_SECONDS * Time.SECOND_IN_NANOSECONDS
@@ -26,15 +30,16 @@ internal abstract class LocationTrackerTimer : TrackerTimerComponent {
 
 	protected fun onNewData(locations: List<Location>) {
 		require(locations.isNotEmpty())
-		val receiver = receiver
-		require(receiver != null)
+		newDataLock.withLock {
+			val receiver = receiver ?: return
 
-		val lastLocation = locations.last()
-		if (lastLocation.elapsedRealtimeNanos + MAX_LOCATION_AGE_IN_NANOS > startedAtElapsedRealtimeNanos) {
-			val tempData = createCollectionTempData(locations)
-			receiver.onUpdate(tempData)
+			val lastLocation = locations.last()
+			if (lastLocation.elapsedRealtimeNanos + MAX_LOCATION_AGE_IN_NANOS > startedAtElapsedRealtimeNanos) {
+				val tempData = createCollectionTempData(locations)
+				receiver.onUpdate(tempData)
 
-			previousLocation = lastLocation
+				previousLocation = lastLocation
+			}
 		}
 	}
 
@@ -58,13 +63,17 @@ internal abstract class LocationTrackerTimer : TrackerTimerComponent {
 
 	@CallSuper
 	override fun onEnable(context: Context, receiver: TrackerTimerReceiver) {
-		this.receiver = receiver
-		startedAtElapsedRealtimeNanos = Time.elapsedRealtimeNanos
+		newDataLock.withLock {
+			this.receiver = receiver
+			startedAtElapsedRealtimeNanos = Time.elapsedRealtimeNanos
+		}
 	}
 
 	@CallSuper
 	override fun onDisable(context: Context) {
-		this.receiver = null
+		newDataLock.withLock {
+			this.receiver = null
+		}
 	}
 
 	companion object {
