117a35102bf4ba487264223424fab7207ddf7d96
adsamcik <adsamcik@users.noreply.github.com>
2019-10-18 19:05:24 +0200

    Add adjustment calculation for speed

--- tracker/src/main/java/com/adsamcik/tracker/tracker/component/consumer/data/LocationTrackerComponent.kt
+++ tracker/src/main/java/com/adsamcik/tracker/tracker/component/consumer/data/LocationTrackerComponent.kt
@@ -1,10 +1,13 @@
 package com.adsamcik.tracker.tracker.component.consumer.data
 
 import android.content.Context
+import android.location.Location
+import com.adsamcik.tracker.common.Time
 import com.adsamcik.tracker.common.data.MutableCollectionData
 import com.adsamcik.tracker.tracker.component.DataTrackerComponent
 import com.adsamcik.tracker.tracker.component.TrackerComponentRequirement
 import com.adsamcik.tracker.tracker.data.collection.CollectionTempData
+import kotlin.math.abs
 
 internal class LocationTrackerComponent : DataTrackerComponent {
 	override val requiredData: Collection<TrackerComponentRequirement> = mutableListOf(
@@ -14,12 +17,40 @@ internal class LocationTrackerComponent : DataTrackerComponent {
 	override suspend fun onDisable(context: Context) = Unit
 	override suspend fun onEnable(context: Context) = Unit
 
+
+	private fun calculateSpeed(prevLocation: Location, location: Location): Float {
+		val recordedSpeed = location.speed
+
+		if (recordedSpeed <= 0f) return calculatedSpeed
+
+		val distance = location.distanceTo(prevLocation)
+		val deltaS = (location.elapsedRealtimeNanos - prevLocation.elapsedRealtimeNanos) / Time.SECOND_IN_NANOSECONDS
+		val calculatedSpeed = distance / deltaS
+
+		val changePercentage = abs(calculatedSpeed - recordedSpeed) / recordedSpeed
+
+		return if (changePercentage >= 0.2f) {
+			calculatedSpeed
+		} else {
+			recordedSpeed
+		}
+	}
+
 	override suspend fun onDataUpdated(
 			tempData: CollectionTempData,
 			collectionData: MutableCollectionData
 	) {
 		val locationResult = tempData.getLocationData(this)
-		collectionData.setLocation(locationResult.lastLocation)
+
+		val location = locationResult.lastLocation
+		val previousLocation = locationResult.previousLocation
+
+		if (previousLocation != null) {
+			val speed = calculateSpeed(previousLocation, location)
+			location.speed = speed
+		}
+
+		collectionData.setLocation(location)
 	}
 }
 
