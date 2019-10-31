392257de4c4ff21da0ec4fb5b7daf50e03c280b1
adsamcik <adsamcik@users.noreply.github.com>
2019-10-31 15:43:02 +0100

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
 		applicationId = "com.adsamcik.tracker"
 		minSdkVersion(Android.min)
 		targetSdkVersion(Android.target)
-		versionCode = 336
-		versionName = "2019.1β13"
+		versionCode = 337
+		versionName = "2019.1β14"
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		resConfigs("en", "cs")
 	}
