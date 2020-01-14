794e393f31b0774753b23fdfa7ad30d28375a758
adsamcik <adsamcik@users.noreply.github.com>
2020-01-14 09:10:01 +0100

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
 	dependencies {
 		classpath("com.android.tools.build:gradle:3.5.0")
 		classpath("com.google.gms:google-services:4.3.3")
-		classpath("com.google.android.gms:oss-licenses-plugin:0.10.0")
+		classpath("com.google.android.gms:oss-licenses-plugin:0.10.1")
 		classpath("io.fabric.tools:gradle:1.31.2")
 
 		classpath("org.jetbrains.dokka:dokka-android-gradle-plugin:${Dependencies.Versions.dokka}")
