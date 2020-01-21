550f17919d12b5bbd57f145f520e70c5f61a6697
adsamcik <adsamcik@users.noreply.github.com>
2020-01-21 17:04:06 +0100

    Add step count consumer

---
+++ statistics/src/main/java/com/adsamcik/tracker/statistics/data/source/consumer/StepCountConsumer.kt
@@ -0,0 +1,35 @@
+package com.adsamcik.tracker.statistics.data.source.consumer
+
+import android.content.Context
+import com.adsamcik.tracker.shared.base.data.TrackerSession
+import com.adsamcik.tracker.statistics.R
+import com.adsamcik.tracker.statistics.data.source.StatDataMap
+import com.adsamcik.tracker.statistics.data.source.abstraction.StatDataDistanceConsumer
+import com.adsamcik.tracker.statistics.data.source.abstraction.StatDataProducer
+import com.adsamcik.tracker.statistics.data.source.producer.TrackerSessionProducer
+import com.adsamcik.tracker.statistics.detail.recycler.StatisticDisplayType
+import com.adsamcik.tracker.statistics.extension.requireData
+import kotlin.reflect.KClass
+
+/**
+ * Distance on foot consumer.
+ */
+class StepCountConsumer : StatDataDistanceConsumer {
+	override val nameRes: Int
+		get() = R.string.stats_steps
+
+	override val iconRes: Int
+		get() = com.adsamcik.tracker.shared.base.R.drawable.ic_shoe_print
+
+	override val displayType: StatisticDisplayType
+		get() = StatisticDisplayType.Information
+
+
+	override fun getDistance(context: Context, data: StatDataMap): Double {
+		val session = data.requireData<TrackerSession>(TrackerSessionProducer::class)
+		return session.steps.toDouble()
+	}
+
+	override val dependsOn: List<KClass<out StatDataProducer>>
+		get() = listOf(TrackerSessionProducer::class)
+}
