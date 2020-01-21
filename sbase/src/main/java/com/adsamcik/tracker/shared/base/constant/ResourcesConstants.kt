a159be29accba082de2a15785dcda8a9a974d8e6
adsamcik <adsamcik@users.noreply.github.com>
2020-01-21 15:18:26 +0100

    Add null resource constant

---
+++ sbase/src/main/java/com/adsamcik/tracker/shared/base/constant/ResourcesConstants.kt
@@ -0,0 +1,11 @@
+package com.adsamcik.tracker.shared.base.constant
+
+/**
+ * Constant resource values.
+ */
+object ResourcesConstants {
+	/**
+	 * Null resources value
+	 */
+	const val ID_NULL = 0
+}
