492f9189cdc46ecaa9cd27fe0b856d2162f055b6
adsamcik <adsamcik@users.noreply.github.com>
2020-06-11 14:01:26 +0200

    Unified API to String type

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -2,7 +2,7 @@
  * Android specific build properties
  */
 object Android {
-	const val min: Int = 21
+	const val min: String = "21"
 	const val compile: String = "android-30"
 	const val target: String = "30"
 
