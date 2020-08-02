7fc2f8c86a1ccbb5fa2709e0a07d24ed01fbe119
adsamcik <adsamcik@users.noreply.github.com>
2020-08-02 18:31:58 +0200

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
 		google()
 	}
 	dependencies {
-		classpath("com.android.tools.build:gradle:4.0.0")
+		classpath("com.android.tools.build:gradle:4.0.1")
 		classpath("com.google.gms:google-services:4.3.3")
 		classpath("com.google.android.gms:oss-licenses-plugin:0.10.2")
 		classpath("com.google.firebase:firebase-crashlytics-gradle:${Dependencies.Versions.crashlyticsGradle}")
@@ -16,7 +16,7 @@ buildscript {
 
 plugins {
 	// gradlew dependencyUpdates -Drevision=release
-	id("com.github.ben-manes.versions") version ("0.28.0")
+	id("com.github.ben-manes.versions") version ("0.29.0")
 }
 
 allprojects {
