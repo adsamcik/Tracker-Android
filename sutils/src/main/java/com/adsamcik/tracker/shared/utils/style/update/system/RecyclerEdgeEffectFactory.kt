817318368f21b90b802897cf0092e4c2a357c3ab
adsamcik <adsamcik@users.noreply.github.com>
2020-08-25 10:56:12 +0200

    Fixed self assignment

--- sutils/src/main/java/com/adsamcik/tracker/shared/utils/style/update/system/RecyclerEdgeEffectFactory.kt
+++ sutils/src/main/java/com/adsamcik/tracker/shared/utils/style/update/system/RecyclerEdgeEffectFactory.kt
@@ -4,11 +4,16 @@ import android.widget.EdgeEffect
 import androidx.annotation.ColorRes
 import androidx.recyclerview.widget.RecyclerView
 
+/**
+ * Edge factory for recycler view
+ */
 class RecyclerEdgeEffectFactory : RecyclerView.EdgeEffectFactory() {
 	@ColorRes
 	var color: Int = 0
 
 	override fun createEdgeEffect(view: RecyclerView, direction: Int): EdgeEffect {
-		return EdgeEffect(view.context).apply { color = color }
+		return EdgeEffect(view.context).apply {
+			color = this@RecyclerEdgeEffectFactory.color
+		}
 	}
 }
