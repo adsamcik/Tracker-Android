ec487907f6307bc4cab967df3e75e932cfce0a1e
adsamcik <adsamcik@users.noreply.github.com>
2020-08-28 13:58:42 +0200

    Refactored ActivityDao

--- sbase/src/main/java/com/adsamcik/tracker/shared/base/database/dao/ActivityDao.kt
+++ sbase/src/main/java/com/adsamcik/tracker/shared/base/database/dao/ActivityDao.kt
@@ -3,38 +3,62 @@ package com.adsamcik.tracker.shared.base.database.dao
 import android.content.Context
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.RewriteQueriesToDropUnusedColumns
 import androidx.room.Transaction
 import com.adsamcik.tracker.shared.base.data.NativeSessionActivity
 import com.adsamcik.tracker.shared.base.data.SessionActivity
 
+/**
+ * Session activity data access object.
+ */
 @Dao
 interface ActivityDao : BaseDao<SessionActivity> {
+	/**
+	 * Get all session activities from database.
+	 */
+	@RewriteQueriesToDropUnusedColumns
 	@Query("SELECT * FROM activity")
 	fun getAll(): List<SessionActivity>
 
+	/**
+	 * Get all session activities created by a user.
+	 */
+	@RewriteQueriesToDropUnusedColumns
 	@Query("SELECT * FROM activity WHERE id >= 0")
 	fun getAllUser(): List<SessionActivity>
 
+	/**
+	 * Get specific session activity.
+	 */
+	@RewriteQueriesToDropUnusedColumns
 	@Query("SELECT * FROM activity WHERE id = :id")
 	fun get(id: Long): SessionActivity?
 
+	/**
+	 * Get specific localized session activity.
+	 */
 	@Transaction
 	fun getLocalized(context: Context, id: Long): SessionActivity? {
-		val sessionActivity = get(id) ?: return null
-
-		if (sessionActivity.id < 0) {
+		return if (id < 0) {
 			val nativeSessionActivity = NativeSessionActivity.values()
-					.find { it.id == sessionActivity.id }
+					.find { it.id == id }
 
-			return requireNotNull(nativeSessionActivity).getSessionActivity(context)
+			requireNotNull(nativeSessionActivity).getSessionActivity(context)
+		} else {
+			get(id)
 		}
-
-		return sessionActivity
 	}
 
+	/**
+	 * Find session activity with exact name.
+	 */
+	@RewriteQueriesToDropUnusedColumns
 	@Query("SELECT * FROM activity WHERE name = :name")
 	fun find(name: String): SessionActivity?
 
+	/**
+	 * Delete specific session activity.
+	 */
 	@Query("DELETE FROM activity WHERE id = :id")
 	fun delete(id: Long)
 }
