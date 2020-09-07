67bfbe8b71e498dd68feaa852c6469b5405c34a8
adsamcik <adsamcik@users.noreply.github.com>
2020-09-07 15:52:05 +0200

    Updated documentation

--- smap/src/main/java/com/adsamcik/tracker/shared/map/MapLayerData.kt
+++ smap/src/main/java/com/adsamcik/tracker/shared/map/MapLayerData.kt
@@ -3,6 +3,10 @@ package com.adsamcik.tracker.shared.map
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
+/**
+ * Layer data.
+ * Contains important description data for layer.
+ */
 @Parcelize
 data class MapLayerData(
 		val info: MapLayerInfo,
