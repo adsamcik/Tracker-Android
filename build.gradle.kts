ebd0e5178a3debf4eae373532b2eda3d6fe3115e
adsamcik <adsamcik@users.noreply.github.com>
2021-02-15 15:53:17 +0100

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
 	}
 	dependencies {
 		classpath("com.android.tools.build:gradle:4.1.2")
-		classpath("com.google.gms:google-services:4.3.4")
+		classpath("com.google.gms:google-services:4.3.5")
 		classpath("com.google.android.gms:oss-licenses-plugin:0.10.2")
 		classpath("com.google.firebase:firebase-crashlytics-gradle:${Dependencies.Versions.crashlyticsGradle}")
 
