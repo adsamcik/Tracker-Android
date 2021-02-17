be81fb35dd3e7ff0dd0d77f2154e09d4db27942a
adsamcik <adsamcik@users.noreply.github.com>
2021-02-17 08:14:59 +0100

    Add documentation

--- sbase/src/main/java/com/adsamcik/tracker/shared/base/database/dao/NotificationPreferenceDao.kt
+++ sbase/src/main/java/com/adsamcik/tracker/shared/base/database/dao/NotificationPreferenceDao.kt
@@ -5,23 +5,39 @@ import androidx.room.Query
 import androidx.room.Transaction
 import com.adsamcik.tracker.shared.base.database.data.NotificationPreference
 
+/**
+ * Data access object for notification preferences
+ */
 @Dao
 interface NotificationPreferenceDao : BaseDao<NotificationPreference> {
+	/**
+	 * Finds all notification preferences
+	 */
 	@Query("SELECT * FROM notification")
 	fun getAll(): List<NotificationPreference>
 
+	/**
+	 * Ensures a notification preference is set to provided value.
+	 *
+	 * @param preference preference value
+	 */
 	@Transaction
-	fun upsert(obj: NotificationPreference) {
-		val id = insert(obj)
+	fun upsert(preference: NotificationPreference) {
+		val id = insert(preference)
 		if (id == -1L) {
-			update(obj)
+			update(preference)
 		}
 	}
 
+	/**
+	 * Ensures all provided notification preferences are set to their values.
+	 *
+	 * @param preferenceList List of preferences
+	 */
 	@Transaction
-	fun upsert(objList: Collection<NotificationPreference>) {
-		val insertResult = insert(objList)
-		val updateList = objList.filterIndexed { index, _ -> insertResult[index] == -1L }
+	fun upsert(preferenceList: Collection<NotificationPreference>) {
+		val insertResult = insert(preferenceList)
+		val updateList = preferenceList.filterIndexed { index, _ -> insertResult[index] == -1L }
 
 		if (updateList.isNotEmpty()) {
 			update(updateList)
--- sutils/src/main/java/com/adsamcik/tracker/shared/utils/extension/WorkExtensions.kt
+++ sutils/src/main/java/com/adsamcik/tracker/shared/utils/extension/WorkExtensions.kt
@@ -1,16 +1,20 @@
 package com.adsamcik.tracker.shared.utils.extension
 
 import androidx.work.Data
-import com.adsamcik.tracker.shared.base.extension.tryGetLong
 import com.adsamcik.tracker.logger.Reporter
+import com.adsamcik.tracker.shared.base.extension.tryGetLong
 
+/**
+ * Tries to get positive long from data.
+ * Returns null and reports it if the value is not found or negative.
+ */
 fun Data.getPositiveLongReportNull(key: String): Long? {
 	val value = getLong(key, -1)
 	return if (value < 0) {
 		if (keyValueMap.contains(key)) {
-			com.adsamcik.tracker.logger.Reporter.report(IllegalArgumentException("Argument $key had invalid negative value of $value"))
+			Reporter.report(IllegalArgumentException("Argument $key had invalid negative value of $value"))
 		} else {
-			com.adsamcik.tracker.logger.Reporter.report(IllegalArgumentException("Argument $key was not specified"))
+			Reporter.report(IllegalArgumentException("Argument $key was not specified"))
 		}
 		null
 	} else {
@@ -18,8 +22,14 @@ fun Data.getPositiveLongReportNull(key: String): Long? {
 	}
 }
 
+/**
+ * Tries to get long from data.
+ * Returns null if it is not found and reports it.
+ */
 fun Data.getLongReportNull(key: String): Long? {
 	val value = tryGetLong(key)
-	if (value == null) com.adsamcik.tracker.logger.Reporter.report(IllegalArgumentException("Argument $key was not specified"))
+	if (value == null) {
+		Reporter.report(IllegalArgumentException("Argument $key was not specified"))
+	}
 	return value
 }
