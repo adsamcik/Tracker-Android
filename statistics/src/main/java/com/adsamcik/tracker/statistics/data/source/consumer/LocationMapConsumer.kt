c8c60e6627155aa5652e80e9f9eb5052a844e774
adsamcik <adsamcik@users.noreply.github.com>
2021-02-18 17:22:30 +0100

    Convert Location to BaseLocation to improve caching

--- statistics/src/main/java/com/adsamcik/tracker/statistics/data/source/consumer/LocationMapConsumer.kt
+++ statistics/src/main/java/com/adsamcik/tracker/statistics/data/source/consumer/LocationMapConsumer.kt
@@ -2,15 +2,14 @@ package com.adsamcik.tracker.statistics.data.source.consumer
 
 import android.content.Context
 import com.adsamcik.tracker.shared.base.constant.ResourcesConstants
+import com.adsamcik.tracker.shared.base.data.BaseLocation
 import com.adsamcik.tracker.shared.base.data.Location
-import com.adsamcik.tracker.statistics.data.adapters.EntryJsonAdapter
 import com.adsamcik.tracker.statistics.data.source.StatDataMap
 import com.adsamcik.tracker.statistics.data.source.abstraction.StatDataConsumer
 import com.adsamcik.tracker.statistics.data.source.abstraction.StatDataProducer
 import com.adsamcik.tracker.statistics.data.source.producer.OptimizedLocationDataProducer
 import com.adsamcik.tracker.statistics.detail.recycler.StatisticDisplayType
 import com.adsamcik.tracker.statistics.extension.requireData
-import com.github.mikephil.charting.data.Entry
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import kotlin.reflect.KClass
@@ -29,6 +28,7 @@ class LocationMapConsumer : StatDataConsumer {
 
 	override fun getData(context: Context, data: StatDataMap): Any {
 		return data.requireData<List<Location>>(OptimizedLocationDataProducer::class)
+				.map { BaseLocation(it) }
 	}
 
 	override val dependsOn: List<KClass<out StatDataProducer>>
@@ -37,14 +37,14 @@ class LocationMapConsumer : StatDataConsumer {
 	override val requiredMoshiAdapter: Any? = null
 
 	override fun serializeData(data: Any, moshi: Moshi): String {
-		val arrayType = Types.newParameterizedType(List::class.java, Location::class.java)
+		val arrayType = Types.newParameterizedType(List::class.java, BaseLocation::class.java)
 		@Suppress("UNCHECKED_CAST")
-		return moshi.adapter<List<Location>>(arrayType).toJson(data as List<Location>)
+		return moshi.adapter<List<BaseLocation>>(arrayType).toJson(data as List<BaseLocation>)
 	}
 
 	override fun deserializeData(data: String, moshi: Moshi): Any {
-		val arrayType = Types.newParameterizedType(List::class.java, Location::class.java)
+		val arrayType = Types.newParameterizedType(List::class.java, BaseLocation::class.java)
 		@Suppress("UNCHECKED_CAST")
-		return moshi.adapter<List<Location>>(arrayType).fromJson(data) as Any
+		return moshi.adapter<List<BaseLocation>>(arrayType).fromJson(data) as Any
 	}
 }
