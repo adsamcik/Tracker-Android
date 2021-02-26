a71726bd8dd503d6053a4f79c537a7a7cfd7ae2c
adsamcik <adsamcik@users.noreply.github.com>
2021-02-26 16:51:18 +0100

    Add Submodule settings

--- spreferences/src/main/java/com/adsamcik/tracker/shared/preferences/ModuleSettings.kt
+++ spreferences/src/main/java/com/adsamcik/tracker/shared/preferences/ModuleSettings.kt
@@ -2,8 +2,22 @@ package com.adsamcik.tracker.shared.preferences
 
 import androidx.preference.PreferenceScreen
 
-interface ModuleSettings {
+/**
+ * Defines structure for dynamic module settings.
+ */
+interface ModuleSettings : SubmoduleSettings {
+	/**
+	 * Resources to module settings icon.
+	 */
 	val iconRes: Int
+}
 
+/**
+ * Utility interface for submodules.
+ */
+interface SubmoduleSettings {
+	/**
+	 * Creates module preference screen.
+	 */
 	fun onCreatePreferenceScreen(preferenceScreen: PreferenceScreen)
 }
