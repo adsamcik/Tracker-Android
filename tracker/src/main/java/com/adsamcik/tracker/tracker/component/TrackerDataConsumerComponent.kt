61bfe1630791fcc84a72a97b2435379763d75f5d
adsamcik <adsamcik@users.noreply.github.com>
2021-02-27 15:54:19 +0100

    Refactoring

--- tracker/src/main/java/com/adsamcik/tracker/tracker/component/TrackerDataConsumerComponent.kt
+++ tracker/src/main/java/com/adsamcik/tracker/tracker/component/TrackerDataConsumerComponent.kt
@@ -2,9 +2,18 @@ package com.adsamcik.tracker.tracker.component
 
 import com.adsamcik.tracker.tracker.data.collection.CollectionTempData
 
+/**
+ * Interface for tracker data consumers.
+ */
 internal interface TrackerDataConsumerComponent {
+	/**
+	 * List of required data
+	 */
 	val requiredData: Collection<TrackerComponentRequirement>
 
+	/**
+	 * Returns true if all requirements are met
+	 */
 	fun requirementsMet(data: CollectionTempData): Boolean {
 		return requiredData.all { it.isRequirementFulfilled(data) }
 	}
