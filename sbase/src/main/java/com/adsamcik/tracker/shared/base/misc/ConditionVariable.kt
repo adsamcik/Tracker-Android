78369e16c40be9d5bcf7c83751181ce49bd1af5e
adsamcik <adsamcik@users.noreply.github.com>
2021-04-11 11:46:14 +0200

    Add documentation

--- sbase/src/main/java/com/adsamcik/tracker/shared/base/misc/ConditionVariable.kt
+++ sbase/src/main/java/com/adsamcik/tracker/shared/base/misc/ConditionVariable.kt
@@ -14,6 +14,13 @@ import kotlin.coroutines.CoroutineContext
 typealias ConditionChecker<T> = (T) -> Boolean
 typealias JobFunction = suspend CoroutineScope.() -> Unit
 
+/**
+ * Conditional variable.
+ * Allows thread-safe conditional access to variable.
+ *
+ * Waiters wait for specified condition to be satisfied.
+ * When condition is satisfied job function associated with the waiter is invoked.
+ */
 open class ConditionVariable<T>(default: T) : CoroutineScope {
 	private val job = SupervisorJob()
 	override val coroutineContext: CoroutineContext
@@ -26,6 +33,9 @@ open class ConditionVariable<T>(default: T) : CoroutineScope {
 
 	protected var unsafeValue: T = default
 
+	/**
+	 * Current value of the variable.
+	 */
 	var value: T
 		get() {
 			return valueLock.read { unsafeValue }
@@ -48,6 +58,9 @@ open class ConditionVariable<T>(default: T) : CoroutineScope {
 		}
 	}
 
+	/**
+	 * Adds waiter and job function associated with it.
+	 */
 	fun addWaiter(checker: ConditionChecker<T>, job: JobFunction) {
 		if (checker.invoke(value)) {
 			launch(block = job)
@@ -59,13 +72,22 @@ open class ConditionVariable<T>(default: T) : CoroutineScope {
 	}
 }
 
+/**
+ * Implementation of condition variable for integers.
+ */
 class ConditionVariableInt(value: Int) : ConditionVariable<Int>(value) {
+	/**
+	 * Increments and returns current value.
+	 */
 	fun incrementAndGet(): Int {
 		val value = valueLock.write { ++unsafeValue }
 		testWaiters(value)
 		return value
 	}
 
+	/**
+	 * Decrements and returns current value.
+	 */
 	fun decrementAndGet(): Int {
 		val value = valueLock.write { --unsafeValue }
 		testWaiters(value)
