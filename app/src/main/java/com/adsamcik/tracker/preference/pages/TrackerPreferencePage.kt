cd45ba3ceb2cae0911be997674e53af2c4c05c09
adsamcik <adsamcik@users.noreply.github.com>
2021-05-08 15:39:43 +0200

    Fixed activity and stepcount not included in validation

--- app/src/main/java/com/adsamcik/tracker/preference/pages/TrackerPreferencePage.kt
+++ app/src/main/java/com/adsamcik/tracker/preference/pages/TrackerPreferencePage.kt
@@ -18,23 +18,29 @@ import com.adsamcik.tracker.tracker.locker.TrackerLocker
 import com.adsamcik.tracker.tracker.notification.NotificationManagementActivity
 import com.adsamcik.tracker.tracker.service.ActivityWatcherService
 
+/**
+ * Tracker preference page.
+ * Contains configuration for tracker options.
+ */
 class TrackerPreferencePage : PreferencePage {
 
 	private lateinit var snackMaker: SnackMaker
 
-	private fun validateEnablePreference(
-			locationEnabled: Boolean,
-			wifiEnabled: Boolean,
-			cellEnabled: Boolean
-	) = locationEnabled.or(wifiEnabled).or(cellEnabled)
 
 	override fun onExit(caller: PreferenceFragmentCompat): Unit = Unit
 
 
 	private fun initializeEnableTrackingPreferences(caller: PreferenceFragmentCompat) {
-		val locationPreference = caller.findPreferenceTyped<CheckBoxPreference>(R.string.settings_location_enabled_key)
-		val wifiPreference = caller.findPreferenceTyped<CheckBoxPreference>(R.string.settings_wifi_enabled_key)
-		val cellPreference = caller.findPreferenceTyped<CheckBoxPreference>(R.string.settings_cell_enabled_key)
+		val locationPreference =
+			caller.findPreferenceTyped<CheckBoxPreference>(R.string.settings_location_enabled_key)
+		val wifiPreference =
+			caller.findPreferenceTyped<CheckBoxPreference>(R.string.settings_wifi_enabled_key)
+		val cellPreference =
+			caller.findPreferenceTyped<CheckBoxPreference>(R.string.settings_cell_enabled_key)
+		val activityPreference =
+			caller.findPreferenceTyped<CheckBoxPreference>(R.string.settings_activity_enabled_key)
+		val stepCountPreference =
+			caller.findPreferenceTyped<CheckBoxPreference>(R.string.settings_steps_enabled_key)
 
 		val context = caller.requireContext()
 		val packageManager = context.packageManager
@@ -46,131 +52,125 @@ class TrackerPreferencePage : PreferencePage {
 			cellPreference.isEnabled = false
 		}
 
-		locationPreference.setOnPreferenceChangeListener { _, newValue ->
-			if (!validateEnablePreference(
-							locationEnabled = newValue as Boolean,
-							wifiEnabled = wifiPreference.isChecked,
-							cellEnabled = cellPreference.isChecked
-					)) {
+		fun validateEnablePreference(
+			locationEnabled: Boolean = locationPreference.isChecked,
+			wifiEnabled: Boolean = wifiPreference.isChecked,
+			cellEnabled: Boolean = cellPreference.isChecked,
+			activity: Boolean = activityPreference.isChecked,
+			stepCount: Boolean = stepCountPreference.isChecked,
+		) = locationEnabled.or(wifiEnabled).or(cellEnabled).or(activity).or(stepCount)
+
+		fun onValidated(isValid: Boolean): Boolean {
+			if (!isValid) {
 				snackMaker.addMessage(
-						R.string.error_nothing_to_track,
-						priority = SnackMaker.SnackbarPriority.IMPORTANT
+					R.string.error_nothing_to_track,
+					priority = SnackMaker.SnackbarPriority.IMPORTANT
 				)
-				false
-			} else {
-				true
 			}
+			return isValid
+		}
+
+		locationPreference.setOnPreferenceChangeListener { _, newValue ->
+			onValidated(validateEnablePreference(locationEnabled = newValue as Boolean))
 		}
 
 		wifiPreference.setOnPreferenceChangeListener { _, newValue ->
-			if (!validateEnablePreference(
-							locationEnabled = locationPreference.isChecked,
-							wifiEnabled = newValue as Boolean,
-							cellEnabled = cellPreference.isChecked
-					)) {
-				snackMaker.addMessage(
-						R.string.error_nothing_to_track,
-						priority = SnackMaker.SnackbarPriority.IMPORTANT
-				)
-				false
-			} else {
-				true
-			}
+			onValidated(validateEnablePreference(wifiEnabled = newValue as Boolean))
 		}
 
 		cellPreference.setOnPreferenceChangeListener { _, newValue ->
-			if (!validateEnablePreference(
-							locationEnabled = locationPreference.isChecked,
-							wifiEnabled = wifiPreference.isChecked,
-							cellEnabled = newValue as Boolean
-					)) {
-				snackMaker.addMessage(
-						R.string.error_nothing_to_track,
-						priority = SnackMaker.SnackbarPriority.IMPORTANT
-				)
-				false
-			} else {
-				true
-			}
+			onValidated(validateEnablePreference(cellEnabled = newValue as Boolean))
+		}
+
+		activityPreference.setOnPreferenceChangeListener { _, newValue ->
+			onValidated(validateEnablePreference(cellEnabled = newValue as Boolean))
+		}
+
+		stepCountPreference.setOnPreferenceChangeListener { _, newValue ->
+			onValidated(validateEnablePreference(cellEnabled = newValue as Boolean))
 		}
 	}
 
 	private fun initializeTrackingTickerPreference(caller: PreferenceFragmentCompat) {
 		caller.findPreferenceTyped<DialogListPreference>(R.string.settings_tracker_timer_key)
-				.apply {
-					val values = TrackerTimerManager.availableTimerData
-					val resources = context.resources
-					val entries = values.map { resources.getString(it.second) }
-					val entryValues = values.map { it.first }
-					setValues(entries, entryValues)
-					val selectedKey = TrackerTimerManager.getSelectedKey(context)
-					val selectedIndex = values.indexOfFirst { it.first == selectedKey }
-					if (selectedIndex >= 0) {
-						setIndex(selectedIndex)
-					} else {
-						Reporter.report("Key $selectedKey was not found in ${entries.joinToString { it }}")
-					}
+			.apply {
+				val values = TrackerTimerManager.availableTimerData
+				val resources = context.resources
+				val entries = values.map { resources.getString(it.second) }
+				val entryValues = values.map { it.first }
+				setValues(entries, entryValues)
+				val selectedKey = TrackerTimerManager.getSelectedKey(context)
+				val selectedIndex = values.indexOfFirst { it.first == selectedKey }
+				if (selectedIndex >= 0) {
+					setIndex(selectedIndex)
+				} else {
+					Reporter.report("Key $selectedKey was not found in ${entries.joinToString { it }}")
 				}
+			}
 	}
 
 	private fun initializeAutoTrackingPreferences(caller: PreferenceFragmentCompat) {
 		caller.findPreference(R.string.settings_tracking_activity_key)
-				.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, newValue ->
-			ActivityWatcherService.onAutoTrackingPreferenceChange(
+			.onPreferenceChangeListener =
+			Preference.OnPreferenceChangeListener { preference, newValue ->
+				ActivityWatcherService.onAutoTrackingPreferenceChange(
 					preference.context,
 					newValue as Int
-			)
-			val context = caller.requireContext()
-			if (newValue > 0) {
-				PermissionManager.checkActivityPermissions(context) {
-					if (!it.isSuccess) {
-						Preferences
+				)
+				val context = caller.requireContext()
+				if (newValue > 0) {
+					PermissionManager.checkActivityPermissions(context) {
+						if (!it.isSuccess) {
+							Preferences
 								.getPref(context)
 								.edit { setInt(R.string.settings_tracking_activity_key, 0) }
-					} else {
-						ActivityWatcherService.poke(context)
+						} else {
+							ActivityWatcherService.poke(context)
+						}
 					}
 				}
+				return@OnPreferenceChangeListener true
 			}
-			return@OnPreferenceChangeListener true
-		}
 
 		caller.findPreference(R.string.settings_activity_watcher_key)
-				.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, newValue ->
-			ActivityWatcherService.onWatcherPreferenceChange(
+			.onPreferenceChangeListener =
+			Preference.OnPreferenceChangeListener { preference, newValue ->
+				ActivityWatcherService.onWatcherPreferenceChange(
 					preference.context,
 					newValue as Boolean
-			)
-			return@OnPreferenceChangeListener true
-		}
-
-		caller.findPreference(R.string.settings_activity_freq_key)
-				.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, newValue ->
-			ActivityWatcherService.onActivityIntervalPreferenceChange(
-					preference.context,
-					newValue as Int
-			)
-			return@OnPreferenceChangeListener true
-		}
-
-		caller.findPreference(R.string.settings_disabled_recharge_key)
-				.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, newValue ->
-			if (newValue as Boolean) {
-				TrackerLocker.lockUntilRecharge(preference.context)
-			} else {
-				TrackerLocker.unlockRechargeLock(preference.context)
+				)
+				return@OnPreferenceChangeListener true
 			}
 
-			return@OnPreferenceChangeListener true
-		}
+		caller.findPreference(R.string.settings_activity_freq_key)
+			.onPreferenceChangeListener =
+			Preference.OnPreferenceChangeListener { preference, newValue ->
+				ActivityWatcherService.onActivityIntervalPreferenceChange(
+					preference.context,
+					newValue as Int
+				)
+				return@OnPreferenceChangeListener true
+			}
+
+		caller.findPreference(R.string.settings_disabled_recharge_key)
+			.onPreferenceChangeListener =
+			Preference.OnPreferenceChangeListener { preference, newValue ->
+				if (newValue as Boolean) {
+					TrackerLocker.lockUntilRecharge(preference.context)
+				} else {
+					TrackerLocker.unlockRechargeLock(preference.context)
+				}
+
+				return@OnPreferenceChangeListener true
+			}
 	}
 
 	private fun initializeNotificationPreference(caller: PreferenceFragmentCompat) {
 		caller.findPreference(R.string.settings_notification_customize_key)
-				.setOnPreferenceClickListener {
-					it.context.startActivity<NotificationManagementActivity> { }
-					false
-				}
+			.setOnPreferenceClickListener {
+				it.context.startActivity<NotificationManagementActivity> { }
+				false
+			}
 	}
 
 	override fun onEnter(caller: PreferenceFragmentCompat) {
