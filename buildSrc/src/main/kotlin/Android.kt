4d8fbda1b56276f0c9a3741432fbe12ade530678
adsamcik <adsamcik@users.noreply.github.com>
2022-11-26 18:56:22 +0100

    Increased android target

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -5,8 +5,8 @@ import org.gradle.api.JavaVersion
  */
 object Android {
 	const val min: Int = 23
-	const val compile: Int = 31
-	const val target: Int = 31
+	const val compile: Int = 33
+	const val target: Int = 33
 
 	const val buildTools: String = "31.0.0"
 
