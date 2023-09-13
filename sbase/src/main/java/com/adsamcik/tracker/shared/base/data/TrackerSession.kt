20797c3f1ef1357dc71178f248594b71a3da8c11
adsamcik <adsamcik@users.noreply.github.com>
2023-09-13 08:24:54 +0200

    Removed protected set as room needs it now

--- sbase/src/main/java/com/adsamcik/tracker/shared/base/data/TrackerSession.kt
+++ sbase/src/main/java/com/adsamcik/tracker/shared/base/data/TrackerSession.kt
@@ -34,39 +34,29 @@ open class TrackerSession(
 
 	@PrimaryKey(autoGenerate = true)
 	open var id: Long = id
-		protected set
 
 	open var start: Long = start
-		protected set
 
 	open var end: Long = end
-		protected set
 
 	@ColumnInfo(name = "user_initiated")
 	open var isUserInitiated: Boolean = isUserInitiated
-		protected set
 
 	open var collections: Int = collections
-		protected set
 
 	@ColumnInfo(name = "distance")
 	open var distanceInM: Float = distanceInM
-		protected set
 
 	@ColumnInfo(name = "distance_on_foot")
 	open var distanceOnFootInM: Float = distanceOnFootInM
-		protected set
 
 	@ColumnInfo(name = "distance_in_vehicle")
 	open var distanceInVehicleInM: Float = distanceInVehicleInM
-		protected set
 
 	open var steps: Int = steps
-		protected set
 
 	@ColumnInfo(name = "session_activity_id")
 	open var sessionActivityId: Long? = sessionActivityId
-		protected set
 
 	companion object {
 		/**
