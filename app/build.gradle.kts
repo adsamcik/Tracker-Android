9c689df5c10d8da4ca7a57397c568c3f41a75d3f
adsamcik <adsamcik@users.noreply.github.com>
2026-02-17 14:32:39 +0100

    Apply kotlin serialization plugin for type-safe navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	alias(libs.plugins.kotlin.android)
 	alias(libs.plugins.kotlin.parcelize)
 	alias(libs.plugins.kotlin.compose)
+	alias(libs.plugins.kotlin.serialization)
 	alias(libs.plugins.ksp)
 	alias(libs.plugins.hilt)
 }
