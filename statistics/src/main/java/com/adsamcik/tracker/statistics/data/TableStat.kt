45086f51193fa8f0f43c97688204659bdb6ad881
adsamcik <adsamcik@users.noreply.github.com>
2026-03-18 10:37:52 +0100

    refactor: remove legacy statistics cache
    Delete the orphaned statistics cache database types that are no longer used
    by the statistics feature pipeline.

--- statistics/src/main/java/com/adsamcik/tracker/statistics/data/TableStat.kt
+++
@@ -1,8 +0,0 @@
-package com.adsamcik.tracker.statistics.data
-
-import com.adsamcik.tracker.statistics.database.data.CacheStatData
-
-/**
- * Object that contains data about specific statistic
- */
-data class TableStat(val name: String, val showPosition: Boolean, val data: List<CacheStatData>)
